d652a437050b54ee43b10d28b79be489d233e4f3
michael.bedward <michael.bedward@07dc4d45-bd82-665a-fa80-c0abab6b2a50>
2009-02-09 02:29:34 +0000

    temporary class while I'm working out what I'm doing with positional functions
    git-svn-id: https://jaitools.googlecode.com/svn@42 07dc4d45-bd82-665a-fa80-c0abab6b2a50

---
+++ trunk/jiffle/src/main/java/jaitools/jiffle/interpreter/Foo.java
@@ -0,0 +1,25 @@
+package jaitools.jiffle.interpreter;
+
+/**
+ * Temporary class - just putting positional methods here before working out how
+ * to fit them into the runtime system
+ * 
+ * @author Michael Bedward
+ */
+public class Foo {
+    public double getX() {
+        return 0;
+    }
+
+    public double getY() {
+        return 0;
+    }
+
+    public double getRow() {
+        return 0;
+    }
+    
+    public double getCol() {
+        return 0;
+    }
+}
