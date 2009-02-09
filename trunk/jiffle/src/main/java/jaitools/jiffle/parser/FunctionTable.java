f1f8370450999d883c6ba23986127437908ff95e
michael.bedward <michael.bedward@07dc4d45-bd82-665a-fa80-c0abab6b2a50>
2009-02-09 02:29:02 +0000

    started adding support for different types of functions: positional, image-info, general
    git-svn-id: https://jaitools.googlecode.com/svn@41 07dc4d45-bd82-665a-fa80-c0abab6b2a50

--- trunk/jiffle/src/main/java/jaitools/jiffle/parser/FunctionTable.java
+++ trunk/jiffle/src/main/java/jaitools/jiffle/parser/FunctionTable.java
@@ -20,8 +20,11 @@
 
 package jaitools.jiffle.parser;
 
+import jaitools.jiffle.collection.CollectionFactory;
+import jaitools.jiffle.interpreter.Foo;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
 
 /**
@@ -31,67 +34,228 @@ import java.util.Random;
  * @author Michael Bedward
  */
 public class FunctionTable {
-    private Random rr = new Random();
-    private HashMap<String, OpBase> lookup = null;
+    
+    /**
+     * Type of function
+     */
+    public enum Type {
+        /** 
+         * General function such as sqrt, rand
+         */
+        GENERAL, 
+        
+        /**
+         * Positional function such as x(), y()
+         */
+        POSITIONAL, 
+        
+        /**
+         * Image info functions such as width()
+         */
+        IMAGE_INFO,
+        
+        /**
+         * User-defined function (not supported at present)
+         */
+        USER;
+    }
+    
+    private static Map<String, Type> typeLookup;
+    
+    static {
+        typeLookup = CollectionFactory.newMap();
+        
+        typeLookup.put("col", Type.POSITIONAL);
+        typeLookup.put("row", Type.POSITIONAL);
+        typeLookup.put("x", Type.POSITIONAL);
+        typeLookup.put("y", Type.POSITIONAL);
+        
+        typeLookup.put("bands", Type.IMAGE_INFO);
+        typeLookup.put("height", Type.IMAGE_INFO);
+        typeLookup.put("width", Type.IMAGE_INFO);
+        
+        typeLookup.put("abs",  Type.GENERAL);
+        typeLookup.put("acos", Type.GENERAL);
+        typeLookup.put("asin", Type.GENERAL);
+        typeLookup.put("atan", Type.GENERAL);
+        typeLookup.put("cos",  Type.GENERAL);
+        typeLookup.put("degToRad", Type.GENERAL);
+        typeLookup.put("log",  Type.GENERAL);
+        typeLookup.put("rand", Type.GENERAL);
+        typeLookup.put("radToDeg", Type.GENERAL);
+        typeLookup.put("randInt", Type.GENERAL);
+        typeLookup.put("sin",  Type.GENERAL);
+        typeLookup.put("sqrt", Type.GENERAL);
+        typeLookup.put("tan",  Type.GENERAL);
+    }
+    
+    
+    static class FunctionTableEntry {
+        Type type;
+        OpBase op;
+        
+        FunctionTableEntry(Type type, OpBase op) {this.type = type; this.op = op;}
+    }
+    
+    private static Random rr = new Random();
+    private static Map<String, FunctionTableEntry> generalFuncLookup = null;
+    
+    static {
+        generalFuncLookup = new HashMap<String, FunctionTableEntry>();
+        
+        generalFuncLookup.put("log",
+                new FunctionTableEntry(
+                Type.POSITIONAL,
+                new OpBase1() {
+                    public double call(double x) {
+                        return Math.log(x);
+                    }
+                }));
 
-    private void initLookup() {
-        lookup = new HashMap<String, OpBase>();
-        lookup.put("log", 
-            new OpBase1(){public double call(double x){return Math.log(x);}});
+        generalFuncLookup.put("sqrt",
+                new FunctionTableEntry(
+                Type.POSITIONAL,
+                new OpBase1() {
 
-        lookup.put("sqrt",
-            new OpBase1(){public double call(double x){return Math.sqrt(x);}});
+                    public double call(double x) {
+                        return Math.sqrt(x);
+                    }
+                }));
 
-        lookup.put("abs",
-            new OpBase1(){public double call(double x){return Math.abs(x);}});
+        generalFuncLookup.put("abs",
+                new FunctionTableEntry(
+                Type.POSITIONAL,
+                new OpBase1() {
 
-        lookup.put("rand",
-            new OpBase1(){public double call(double x){return rr.nextDouble()*x;}});
+                    public double call(double x) {
+                        return Math.abs(x);
+                    }
+                }));
 
-        lookup.put("randInt",
-            new OpBase1(){public double call(double x){return rr.nextInt((int)x);}});
+        generalFuncLookup.put("rand",
+                new FunctionTableEntry(
+                Type.POSITIONAL,
+                new OpBase1() {
+                    public double call(double x) {
+                        return rr.nextDouble() * x;
+                    }
+                }));
 
-        lookup.put("sin",
-            new OpBase1(){public double call(double x){return Math.sin(x);}});
+        generalFuncLookup.put("randInt",
+                new FunctionTableEntry(
+                Type.POSITIONAL,
+                new OpBase1() {
+                    public double call(double x) {
+                        return rr.nextInt((int) x);
+                    }
+                }));
 
-        lookup.put("cos",
-            new OpBase1(){public double call(double x){return Math.cos(x);}});
+        generalFuncLookup.put("sin",
+                new FunctionTableEntry(
+                Type.POSITIONAL,
+                new OpBase1() {
+                    public double call(double x) {
+                        return Math.sin(x);
+                    }
+                }));
 
-        lookup.put("tan",
-            new OpBase1(){public double call(double x){return Math.tan(x);}});
+        generalFuncLookup.put("cos",
+                new FunctionTableEntry(
+                Type.POSITIONAL,
+                new OpBase1() {
+                    public double call(double x) {
+                        return Math.cos(x);
+                    }
+                }));
 
-        lookup.put("asin",
-            new OpBase1(){public double call(double x){return Math.asin(x);}});
+        generalFuncLookup.put("tan",
+                new FunctionTableEntry(
+                Type.POSITIONAL,
+                new OpBase1() {
+                    public double call(double x) {
+                        return Math.tan(x);
+                    }
+                }));
 
-        lookup.put("acos",
-            new OpBase1(){public double call(double x){return Math.acos(x);}});
+        generalFuncLookup.put("asin",
+                new FunctionTableEntry(
+                Type.POSITIONAL,
+                new OpBase1() {
+                    public double call(double x) {
+                        return Math.asin(x);
+                    }
+                }));
 
-        lookup.put("atan",
-            new OpBase1(){public double call(double x){return Math.atan(x);}});
+        generalFuncLookup.put("acos",
+                new FunctionTableEntry(
+                Type.POSITIONAL,
+                new OpBase1() {
+                    public double call(double x) {
+                        return Math.acos(x);
+                    }
+                }));
 
-        lookup.put("degToRad",
-            new OpBase1(){public double call(double x){return Math.PI * x / 180d;}});
+        generalFuncLookup.put("atan",
+                new FunctionTableEntry(
+                Type.POSITIONAL,
+                new OpBase1() {
+                    public double call(double x) {
+                        return Math.atan(x);
+                    }
+                }));
 
-        lookup.put("radToDeg",
-            new OpBase1(){public double call(double x){return x / Math.PI * 180d;}});
+        generalFuncLookup.put("degToRad",
+                new FunctionTableEntry(
+                Type.POSITIONAL,
+                new OpBase1() {
+                    public double call(double x) {
+                        return Math.PI * x / 180d;
+                    }
+                }));
 
+        generalFuncLookup.put("radToDeg",
+                new FunctionTableEntry(
+                Type.POSITIONAL,
+                new OpBase1() {
+                    public double call(double x) {
+                        return x / Math.PI * 180d;
+                    }
+                }));
+
+    }
+
+    /*
+     * @todo Foo is a holder for methods where I haven't decided
+     * where they should live yet
+     */
+    private Foo foo;
+
+    public static Type getFunctionType(String name) {
+        return typeLookup.get(name);
     }
 
     /**
-     * Get a function by name.
-     * @param name the function name (case-sensitive)
-     * @return an OpBase ref for the function or null if the function
-     * has not been defined
+     * Constructor. Use this when position and image-info functions are
+     * not required, e.g. direct evaluation of simple statements
      */
-    private OpBase getOp(String name) {
-        if (lookup == null) {
-            initLookup();
-        }
-        return lookup.get(name);
+    public FunctionTable() {
+        
     }
     
+    /**
+     * Constructor to be used when a jiffle script is being run by a 
+     * JiffleInterpreter. Enables used of position and image info
+     * functions.
+     * 
+     * @param foo ????
+     */
+    public FunctionTable(Foo foo) {
+        this.foo = foo;
+    }
+    
+    
     boolean isDefined(String name, int numArgs) {
-        OpBase op = getOp(name);
+        OpBase op = generalFuncLookup.get(name).op;
         if (op == null) {
             return false;
         }
@@ -100,50 +264,29 @@ public class FunctionTable {
     }
     
     double invoke(String name, List<Double> args) {
+        OpBase op = null;
+        Type t = typeLookup.get(name);
+        if (t == Type.GENERAL) {
+            op = generalFuncLookup.get(name).op;
+        } else {
+            // @todo WRITE ME !
+            return 0;
+        }
+        
         switch (args.size()) {
             case 0:
-                return invoke(name);
+                return ((OpBase0)op).call();
                 
             case 1:
-                return invoke(name, args.get(0));
+                return ((OpBase1)op).call(args.get(0));
                 
             case 2:
-                return invoke(name, args.get(0), args.get(1));
+                return ((OpBase2)op).call(args.get(0), args.get(1));
                 
             default:
                 throw new IllegalStateException(
                         "unsupported function: " + name + " with " + args.size() + " args");
         }
-    }
-
-    /**
-     * Invoke a no argument function.
-     * @param name the function name
-     * @return result as a double
-     */
-    private double invoke(String name) {
-        return ((OpBase0)getOp(name)).call();
-    }
-
-    /**
-     * Invoke a single argument function.
-     * @param name the function name
-     * @param x argument value
-     * @return result as a double
-     */
-    private double invoke(String name, double x) {
-        return ((OpBase1)getOp(name)).call(x);
-    }
-
-    /**
-     * Invoke a two argument function.
-     * @param name the function name
-     * @param x1 first argument value
-     * @param x2 second argument value
-     * @return result as a double
-     */
-    private double invoke(String name, double x1, double x2) {
-        return ((OpBase2)getOp(name)).call(x1, x2);
     }
 
 }
