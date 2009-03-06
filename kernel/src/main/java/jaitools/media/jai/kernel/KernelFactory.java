dab2a4351f6efdb13344d4f45247827e129975ae
michael.bedward <michael.bedward@07dc4d45-bd82-665a-fa80-c0abab6b2a50>
2009-03-06 13:15:59 +0000

    renamed artifact and module dir; added KernelUtil class; more unit tests
    git-svn-id: https://jaitools.googlecode.com/svn/trunk@196 07dc4d45-bd82-665a-fa80-c0abab6b2a50

---
+++ kernel/src/main/java/jaitools/media/jai/kernel/KernelFactory.java
@@ -0,0 +1,249 @@
+/*
+ * Copyright 2009 Michael Bedward
+ *
+ * This file is part of jai-tools.
+
+ * jai-tools is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+
+ * jai-tools is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with jai-tools.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+package jaitools.media.jai.kernel;
+
+import javax.media.jai.KernelJAI;
+
+/**
+ * A factory class with static methods to create a variety of
+ * KernelJAI configurations
+ *
+ * @author Michael Bedward
+ *
+ */
+public class KernelFactory {
+
+    /**
+     * Meaning of the kernel values
+     */
+    public static enum ValueType {
+
+        /**
+         * Simple binary kernel with values of 1 or 0
+         */
+        BINARY,
+        /**
+         * The value of each kernel element is its distance to the
+         * kernel's key element (the element that is placed over
+         * image pixels during kernel operations).
+         */
+        DISTANCE,
+        /**
+         * The value of each kernel element is the inverse distance to the
+         * kernel's key element (the element that is placed over
+         * image pixels during kernel operations).
+         */
+        INVERSE_DISTANCE;
+    };
+
+    /**
+     * Creates a new KernelJAI object with a pseudo-circular configuration.
+     * The kernel width is 2*radius + 1.
+     * The kernel's key element is at position x=radius, y=radius
+     *
+     * @param radius the radius of the circle expressed in pixels
+     *
+     * @param type one of
+     * {@linkplain ValueType#BINARY},
+     * {@linkplain ValueType#DISTANCE} or
+     * {@linkplain ValueType#INVERSE_DISTANCE}
+     *
+     * @param centreValue the value to assign to the kernel centre (key element)
+     * 
+     * @return a new instance of KernelJAI
+     */
+    public static KernelJAI createCircle(int radius, ValueType type, float centreValue) {
+
+        if (radius <= 0) {
+            throw new IllegalArgumentException(
+                    "Invalid radius (" + radius + "); must be > 0");
+        }
+
+        int width = 2 * radius + 1;
+        float[] weights = new float[width * width];
+
+        int r2 = radius * radius;
+        int k0 = 0;
+        int k1 = weights.length - 1;
+
+        for (int y = radius; y > 0; y--) {
+            int y2 = y * y;
+            for (int x = -radius; x <= radius; x++, k0++, k1--) {
+                float dist2 = x * x + y2;
+                float value = 0f;
+
+                if (fcomp(r2, dist2) >= 0) {
+                    if (type == ValueType.DISTANCE) {
+                        value = (float) Math.sqrt(dist2);
+                    } else if (type == ValueType.INVERSE_DISTANCE) {
+                        value = 1.0f / (float) Math.sqrt(dist2);
+                    } else {
+                        value = 1.0f;
+                    }
+
+                    weights[k0] = weights[k1] = value;
+                }
+            }
+        }
+
+        for (int x = -radius; x <= radius; x++, k0++) {
+            float value;
+            if (x == 0) {
+                value = centreValue;
+            } else {
+                if (type == ValueType.DISTANCE) {
+                    value = (float) Math.sqrt(x * x);
+                } else if (type == ValueType.INVERSE_DISTANCE) {
+                    value = 1.0f / (float) Math.sqrt(x * x);
+                } else {
+                    value = 1.0f;
+                }
+            }
+            weights[k0] = value;
+        }
+
+        return new KernelJAI(width, width, weights);
+    }
+
+    /**
+     * Creates a new KernelJAI object with an annular configuration
+     * (like a doughnut).
+     *
+     * The kernel width is 2*outerRadius + 1.
+     * The kernel's key element is at position x=outerRadius, y=outerRadius
+     *
+     * <p>
+     * An IllegalArgumentException will be thrown if:
+     * <ul>
+     * <li> The value of outerRadius not greater than 0
+     * <li> The value of innerRadius is not less than outerRadius
+     * </ul>
+     *
+     * Calling this method with innerRadius == 0 is equivalent to
+     * calling {@linkplain #createCircle }
+     *
+     * @param outerRadius the radius of the circle expressed in pixels
+     * @param innerRadius the radius of the 'hole' of the annulus
+     *
+     * @param type one of
+     * {@linkplain ValueType#BINARY},
+     * {@linkplain ValueType#DISTANCE} or
+     * {@linkplain ValueType#INVERSE_DISTANCE}
+     *
+     * @param centreValue the value to assign to the kernel centre (key element)
+     *
+     * @return a new instance of KernelJAI
+     *
+     */
+    public static KernelJAI createAnnulus(int outerRadius, int innerRadius, ValueType type, float centreValue) {
+
+        if (innerRadius < 0) {
+            throw new IllegalArgumentException(
+                    "Invalid innerRadius (" + innerRadius + "); must be >= 0");
+        }
+
+        if (outerRadius <= innerRadius) {
+            throw new IllegalArgumentException("outerRadius must be greater than innerRadius");
+        }
+
+        if (innerRadius == 0) {
+            return createCircle(outerRadius, type, centreValue);
+        }
+
+        int width = 2 * outerRadius + 1;
+        float[] weights = new float[width * width];
+
+        int outer2 = outerRadius * outerRadius;
+        int inner2 = innerRadius * innerRadius;
+        int k0 = 0;
+        int k1 = weights.length - 1;
+
+        for (int y = outerRadius; y > 0; y--) {
+            int y2 = y * y;
+            for (int x = -outerRadius; x <= outerRadius; x++, k0++, k1--) {
+                float dist2 = x * x + y2;
+                float value = 0f;
+
+                if (fcomp(dist2, outer2) <= 0 && fcomp(dist2, inner2) > 0) {
+                    if (type == ValueType.DISTANCE) {
+                        value = (float) Math.sqrt(dist2);
+                    } else if (type == ValueType.INVERSE_DISTANCE) {
+                        value = 1.0f / (float) Math.sqrt(dist2);
+                    } else {
+                        value = 1.0f;
+                    }
+
+                    weights[k0] = weights[k1] = value;
+                }
+            }
+        }
+
+        for (int x = -outerRadius; x <= outerRadius; x++, k0++) {
+            float value = 0f;
+            if (x == 0) {
+                value = centreValue;
+
+            } else if (x < -innerRadius || x > innerRadius) {
+
+                if (type == ValueType.DISTANCE) {
+                    value = (float) Math.sqrt(x * x);
+                } else if (type == ValueType.INVERSE_DISTANCE) {
+                    value = 1.0f / (float) Math.sqrt(x * x);
+                } else {
+                    value = 1.0f;
+                }
+            }
+            
+            weights[k0] = value;
+        }
+
+        return new KernelJAI(width, width, weights);
+    }
+
+    // round-off tolerance: used in the fcomp method below
+    private static final float TOL = 1.0e-8f;
+
+    /**
+     * Equivalent to {@linkplain java.lang.Float#compare(float, float) } but
+     * with a round-off tolerance
+     * @param f1 first value
+     * @param f2 second value
+     * @return 0 if f1 and f2 are equal; less than 0 if f1 is less than f2;
+     * greater than 0 if f1 is greater than f2
+     */
+    private static int fcomp(float f1, float f2) {
+        if (Math.abs(f1 - f2) < TOL) {
+            return 0;
+        } else {
+            return Float.compare(f1, f2);
+        }
+    }
+
+    /**
+     * Test if two float values are equal, taking into accont a
+     * round-off tolerance
+     * @param f1 first value
+     * @param f2 second value
+     * @return true if equal, false otherwise
+     */
+    private static boolean feq(float f1, float f2) {
+        return Math.abs(f1 - f2) < TOL;
+    }
+}
