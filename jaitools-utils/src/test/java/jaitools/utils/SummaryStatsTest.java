d84a49e3021ff1fbc55520a3d36710920534a050
michael.bedward <michael.bedward@07dc4d45-bd82-665a-fa80-c0abab6b2a50>
2009-03-11 07:54:50 +0000

    moved utility classes out of Jiffle module and into a separate module that other jai-tools modules can use
    git-svn-id: https://jaitools.googlecode.com/svn/trunk@223 07dc4d45-bd82-665a-fa80-c0abab6b2a50

---
+++ jaitools-utils/src/test/java/jaitools/utils/SummaryStatsTest.java
@@ -0,0 +1,115 @@
+/*
+ * Copyright 2009 Michael Bedward
+ * 
+ * This file is part of jai-tools.
+
+ * jai-tools is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the 
+ * License, or (at your option) any later version.
+
+ * jai-tools is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+
+ * You should have received a copy of the GNU Lesser General Public 
+ * License along with jai-tools.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
+package jaitools.utils;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Michael Bedward
+ */
+public class SummaryStatsTest {
+    
+    static final Double[] values = {-5d, -2d, 1d, 1d, 1d, 3d, 3d, 3d, 3d, 3d, Double.NaN, 5d};
+
+    @Test
+    public void testMax() {
+        System.out.println("max");
+        double expResult = 5.0;
+        double result = SummaryStats.max(values, true);
+        assertEquals(expResult, result);
+    }
+
+    @Test
+    public void testMin() {
+        System.out.println("min");
+        double expResult = -5.0;
+        double result = SummaryStats.min(values, true);
+        assertEquals(expResult, result);
+    }
+
+    @Test
+    public void testMedian() {
+        System.out.println("median");
+        double expResult = 3d;
+        double result = SummaryStats.median(values, true);
+        assertEquals(expResult, result);
+    }
+    
+    @Test
+    public void testMode() {
+        System.out.println("mode");
+        double expResult = 3.0d;
+        double result = SummaryStats.mode(values, true);
+        assertEquals(expResult, result);
+    }
+
+    @Test
+    public void testRange() {
+        System.out.println("range");
+        double expResult = 10.0d;
+        double result = SummaryStats.range(values, true);
+        assertEquals(expResult, result);
+    }
+    
+    @Test
+    public void testMean() {
+        System.out.println("mean");
+        assertEquals(calcMean(), SummaryStats.mean(values, true));
+    }
+
+    @Test
+    public void testVariance() {
+        System.out.println("variance");
+        double v1 = calcVariance();
+        double v2 = SummaryStats.variance(values, true);
+        assertTrue(DoubleComparison.dcomp(v1, v2) == 0);
+    }
+
+    private double calcMean() {
+        int n = 0;
+        double sum = 0.0d;
+        for (int i = 0; i < values.length; i++) {
+            if (!Double.isNaN(values[i])) {
+                sum += values[i];
+                n++ ;
+            }
+        }
+        return sum / n;
+    }
+
+    private double calcVariance() {
+        int n = 0;
+        double var = 0.0d;
+        double mean = calcMean();
+
+        for (int i = 0; i < values.length; i++) {
+            if (!Double.isNaN(values[i])) {
+                var += (values[i] - mean)*(values[i] - mean);
+                n++ ;
+            }
+        }
+
+        return var / (n-1);
+    }
+
+}
