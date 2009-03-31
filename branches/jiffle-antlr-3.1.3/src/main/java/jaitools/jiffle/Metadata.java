3bccc95adf44d33a028510848fe1e44786b11f87
michael.bedward <michael.bedward@07dc4d45-bd82-665a-fa80-c0abab6b2a50>
2009-03-31 01:13:34 +0000

    branch for migration to ANTLR 3.1.3
    git-svn-id: https://jaitools.googlecode.com/svn@316 07dc4d45-bd82-665a-fa80-c0abab6b2a50

---
+++ branches/jiffle-antlr-3.1.3/src/main/java/jaitools/jiffle/Metadata.java
@@ -0,0 +1,104 @@
+/*
+ * Copyright 2009 Michael Bedward
+ * 
+ * This file is part of jai-tools.
+
+ * jai-tools is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the 
+ * License, or (at your option) any later version.
+
+ * jai-tools is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+
+ * You should have received a copy of the GNU Lesser General Public 
+ * License along with jai-tools.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
+package jaitools.jiffle;
+
+import jaitools.jiffle.parser.VarClassifier;
+import java.awt.Rectangle;
+import java.util.Collections;
+import java.util.Map;
+import java.util.Set;
+import javax.media.jai.TiledImage;
+
+/**
+ * A container for information about variables gathered by 
+ * {@link jaitools.jiffle.parser.VarClassifier}. Not intended for
+ * use in client code.
+ * 
+ * @author Michael Bedward
+ */
+public class Metadata {
+    
+    private Map<String, TiledImage> imageParams;
+    private Rectangle outImgBounds;
+    private Set<String> outImgVars;
+    private Set<String> localVars;
+    private Set<String> positionalVars;
+    private Set<String> userVars;
+
+    /**
+     * Package private constructor
+     * 
+     * @param imageParams Map of variable : image object supplied by the user
+     * with the script
+     */
+    Metadata(Map<String, TiledImage> imageParams) {
+        this.imageParams = imageParams;
+    }
+
+    /**
+     * Collect data on variables gathered by {@link jaitools.jiffle.parser.VarClassifier}
+     * 
+     * @param classifier the VarClassifier object
+     */
+    public void setVarData(VarClassifier classifier) {
+        outImgVars = classifier.getOutputImageVars();
+        localVars = classifier.getLocalVars();
+        positionalVars = classifier.getPositionalVars();
+        userVars = classifier.getUserVars();
+    }
+    
+    /**
+     * Returns an unmodifiable view of the image parameters
+     */
+    public Map<String, TiledImage> getImageParams() {
+        return Collections.unmodifiableMap(imageParams);
+    }
+    
+    /**
+     * Retuns the image variable names as an unmodifiable Set
+     */
+    public Set<String> getImageVars() {
+        return Collections.unmodifiableSet(imageParams.keySet());
+    }
+    
+    /**
+     * Returns the names of local variables as an unmodifiable Set
+     */
+    public Set<String> getLocalVars() {
+        return Collections.unmodifiableSet(localVars);
+    }
+
+    /**
+     * Returns the names of output image variables as an unmodifiable Set
+     */
+    public Set<String> getOutputImageVars() {
+        return Collections.unmodifiableSet(outImgVars);
+    }
+
+    /**
+     * Returns the names of variables that depend on image position
+     * as an unmodifiable Set
+     */
+    public Set<String> getPositionalVars() {
+        return Collections.unmodifiableSet(positionalVars);
+    }
+
+}
