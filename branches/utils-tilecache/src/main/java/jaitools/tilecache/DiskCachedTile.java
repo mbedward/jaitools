3360dbf0de73d093c631da0d1d26804ae03b36c6
michael.bedward <michael.bedward@07dc4d45-bd82-665a-fa80-c0abab6b2a50>
2009-07-02 08:01:55 +0000

    decided to try wrapping more logic into the cached tile class rather than having the TileCache do everything with dumb objects
    git-svn-id: https://jaitools.googlecode.com/svn@410 07dc4d45-bd82-665a-fa80-c0abab6b2a50

--- branches/utils-tilecache/src/main/java/jaitools/tilecache/DiskCachedTile.java
+++ branches/utils-tilecache/src/main/java/jaitools/tilecache/DiskCachedTile.java
@@ -1,6 +1,21 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright 2009 Michael Bedward
+ *
+ * This file is part of jai-tools.
+ *
+ * jai-tools is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * jai-tools is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with jai-tools.  If not, see <http://www.gnu.org/licenses/>.
+ *
  */
 
 package jaitools.tilecache;
@@ -15,16 +30,9 @@ import javax.media.jai.CachedTile;
 import javax.media.jai.PlanarImage;
 
 /**
- * Information associated with a cached tile.
+ * Represents a tile cached on disk that may also be resident in memory
  *
- * <p> This class is used by SunTileCache to create an object that
- * includes all the information associated with a tile, and is put
- * into the tile cache.
- *
- * <p> It also serves as a double linked list.
- *
- * @see SunTileCache
- *
+ * @see DiskBasedTileCache
  */
 final class DiskCachedTile implements CachedTile {
 
@@ -38,10 +46,7 @@ final class DiskCachedTile implements CachedTile {
     private Object key;
     private long memorySize;
 
-    private DiskCachedTile previous;
-    private DiskCachedTile next;
-
-    private int action = 0;             // add, remove, update from tile cache
+    private int action = 0;
 
 
     DiskCachedTile(RenderedImage owner,
@@ -62,44 +67,11 @@ final class DiskCachedTile implements CachedTile {
         this.tileCacheMetric = tileCacheMetric;
         this.file = file;
 
-        key = hashKey(owner, tileX, tileY);
+        key = generateTileKey(owner, tileX, tileY);
 
         DataBuffer db = tile.getDataBuffer();
         memorySize = DataBuffer.getDataTypeSize(db.getDataType()) / 8L *
                      db.getSize() * db.getNumBanks();
-    }
-
-    /**
-     * Generate a hash key for this tile. This uses the same technique as the
-     * Sun memory cache implementation: putting the id of the owning image
-     * into the upper bytes of a long or BigInteger value and the tile id into
-     * the lower bytes.
-     */
-    static Object hashKey(RenderedImage owner,
-                              int tileX,
-                              int tileY) {
-
-        long tileId = tileY * (long)owner.getNumXTiles() + tileX;
-
-        BigInteger imageId = null;
-
-        if (owner instanceof PlanarImage) {
-            imageId = (BigInteger)((PlanarImage)owner).getImageID();
-        }
-
-        if (imageId != null) {
-            byte[] buf = imageId.toByteArray();
-            int length = buf.length;
-            byte[] buf1 = new byte[buf.length + 8];
-            System.arraycopy(buf, 0, buf1, 0, length);
-            for (int i = 7, j = 0; i >= 0; i--, j += 8)
-                buf1[length++] = (byte)(tileId >> j);
-            return new BigInteger(buf1);
-
-        } else {
-            tileId &= 0x00000000ffffffffL;
-            return new Long(((long)owner.hashCode() << 32) | tileId);
-        }
     }
 
     /**
@@ -135,27 +107,93 @@ final class DiskCachedTile implements CachedTile {
         throw new UnsupportedOperationException("Can't get a tile directly from a DiskCachedTile object");
     }
 
+    /**
+     * Get the image that owns this tile
+     */
     public RenderedImage getOwner() {
         return owner.get();
     }
 
+    /**
+     * Get the last time of access for this tile
+     */
     public long getTileTimeStamp() {
         return timeStamp;
     }
+
 
     public Object getTileCacheMetric() {
         return tileCacheMetric;
     }
 
+    /**
+     * Get the tile size in bytes. This is actually the
+     * size of the raster associated with this cached tile instance.
+     */
     public long getTileSize() {
         return memorySize;
     }
 
+    /**
+     * Not used at present - returns 0
+     */
     public int getAction() {
         return action;
     }
 
-    public File getFile() {
+    /**
+     * Get the file used to cache this tile on disk.
+     * @todo Perhaps we should be using a URL ?
+     */
+    public File getSource() {
         return file;
     }
+
+    /**
+     * Get the unique key for this tile. The key is a combination of
+     * either the JAI-generated unique ID of the owning image or the image's
+     * hash key if a unique ID was not available plus the tile index.
+     * The returned object will be either Long or BigInteger.
+     */
+    public Object getTileKey() {
+        return key;
+    }
+
+    /**
+     * Generate a hash key for this tile. This uses the same technique as the
+     * Sun memory cache implementation: putting the id of the owning image
+     * into the upper bytes of a long or BigInteger value and the tile id into
+     * the lower bytes.
+     */
+    private Object generateTileKey(RenderedImage owner,
+                              int tileX,
+                              int tileY) {
+
+        if (key != null) {
+            throw new IllegalStateException("trying to generate the tile key more than once");
+        }
+
+        long tileId = tileY * (long)owner.getNumXTiles() + tileX;
+
+        BigInteger imageId = null;
+
+        if (owner instanceof PlanarImage) {
+            imageId = (BigInteger)((PlanarImage)owner).getImageID();
+        }
+
+        if (imageId != null) {
+            byte[] buf = imageId.toByteArray();
+            int length = buf.length;
+            byte[] buf1 = new byte[buf.length + 8];
+            System.arraycopy(buf, 0, buf1, 0, length);
+            for (int i = 7, j = 0; i >= 0; i--, j += 8)
+                buf1[length++] = (byte)(tileId >> j);
+            return new BigInteger(buf1);
+
+        } else {
+            tileId &= 0x00000000ffffffffL;
+            return new Long(((long)owner.hashCode() << 32) | tileId);
+        }
+    }
+
 }
