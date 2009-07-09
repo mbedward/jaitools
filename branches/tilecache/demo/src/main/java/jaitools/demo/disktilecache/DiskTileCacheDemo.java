e8e94f3798db4ccb00dd1019d583ffbaf8ab799b
michael.bedward <michael.bedward@07dc4d45-bd82-665a-fa80-c0abab6b2a50>
2009-07-09 08:00:01 +0000

    reorganizing branch
    git-svn-id: https://jaitools.googlecode.com/svn@433 07dc4d45-bd82-665a-fa80-c0abab6b2a50

---
+++ branches/tilecache/demo/src/main/java/jaitools/demo/disktilecache/DiskTileCacheDemo.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright 2009 Michael Bedward
+ *
+ * This file is part of jai-tools.
+ *
+ * jai-tools is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * jai-tools is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with jai-tools.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package jaitools.demo.disktilecache;
+
+import jaitools.tilecache.DiskBasedTileCache;
+import java.awt.RenderingHints;
+import java.awt.image.Raster;
+import java.util.HashMap;
+import java.util.Map;
+import javax.media.jai.ImageLayout;
+import javax.media.jai.JAI;
+import javax.media.jai.ParameterBlockJAI;
+import javax.media.jai.RenderedOp;
+
+/**
+ * Testing and demonstration of {@linkplain jaitools.tilecache.DiskBasedTileCache}
+ *
+ * @author Michael Bedward
+ */
+public class DiskTileCacheDemo {
+
+    private static final int IMAGE_WIDTH = 256;
+    private static final int IMAGE_HEIGHT = 384;
+    private static final int TILE_WIDTH = 128;
+
+    public static void main(String[] args) {
+        DiskTileCacheDemo me = new DiskTileCacheDemo();
+        me.demo();
+    }
+
+    private void demo() {
+        Map<String, Object> cacheParams = new HashMap<String, Object>();
+        cacheParams.put(DiskBasedTileCache.KEY_INITIAL_MEMORY_CAPACITY, 100L * 1024 * 1024);
+        cacheParams.put(DiskBasedTileCache.KEY_NEW_TILES_RESIDENT, DiskBasedTileCache.VALUE_NEW_TILES_RESIDENT_TRY);
+        cacheParams.put(DiskBasedTileCache.KEY_USE_MEMORY_THRESHOLD, Boolean.TRUE);
+
+        DiskBasedTileCache cache = new DiskBasedTileCache(cacheParams);
+
+        ParameterBlockJAI pb = new ParameterBlockJAI("constant");
+        pb.setParameter("width", (float)IMAGE_WIDTH);
+        pb.setParameter("height", (float)IMAGE_HEIGHT);
+        pb.setParameter("bandValues", new Double[]{0d, 1d, 2d});
+
+        ImageLayout layout = new ImageLayout();
+        layout.setTileWidth(TILE_WIDTH);
+        layout.setTileHeight(TILE_WIDTH);
+
+        Map<RenderingHints.Key, Object> imgParams = new HashMap<RenderingHints.Key, Object>();
+        imgParams.put(JAI.KEY_IMAGE_LAYOUT, layout);
+        imgParams.put(JAI.KEY_TILE_CACHE, cache);
+
+        RenderingHints hints = new RenderingHints(imgParams);
+        RenderedOp op1 = JAI.create("constant", pb, hints);
+
+        pb = new ParameterBlockJAI("MultiplyConst");
+        pb.setSource("source0", op1);
+        pb.setParameter("constants", new double[]{2.0d});
+
+        RenderedOp op2 = JAI.create("MultiplyConst", pb, hints);
+
+        // force computation
+        Raster[] tiles = op2.getTiles();
+    }
+}
