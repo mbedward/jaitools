f29df7778caf4ce0aef72af0fec4e15c62687785
michael.bedward <michael.bedward@07dc4d45-bd82-665a-fa80-c0abab6b2a50>
2009-07-14 09:35:29 +0000

    resurrecting deleted branch r453 to fix stuff-up in merges into trunk earlier today
    git-svn-id: https://jaitools.googlecode.com/svn@474 07dc4d45-bd82-665a-fa80-c0abab6b2a50

---
+++ branches/tilecache/utils/src/main/java/jaitools/utils/FrameWithStatusBar.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package jaitools.utils;
+
+/**
+ *
+ * @author Michael Bedward
+ */
+public interface FrameWithStatusBar {
+
+    public void setStatusText(String text);
+    
+}
