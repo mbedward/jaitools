7ff7f56538bfc33aa08e16e941d252396c369213
michael.bedward <michael.bedward@07dc4d45-bd82-665a-fa80-c0abab6b2a50>
2009-09-02 03:00:33 +0000

    initial floodfill op code copied from regionalize op
    git-svn-id: https://jaitools.googlecode.com/svn@545 07dc4d45-bd82-665a-fa80-c0abab6b2a50

---
+++ branches/new-ops/floodfill/src/main/java/jaitools/media/jai/floodfill/FloodFillDescriptor.java
@@ -0,0 +1,167 @@
+/*
+ * Copyright 2009 Michael Bedward
+ * 
+ * This file is part of jai-tools.
+
+ * jai-tools is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the 
+ * License, or (at your option) any later version.
+
+ * jai-tools is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+
+ * You should have received a copy of the GNU Lesser General Public 
+ * License along with jai-tools.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
+package jaitools.media.jai.floodfill;
+
+import java.awt.RenderingHints;
+import java.awt.image.RenderedImage;
+import javax.media.jai.JAI;
+import javax.media.jai.OperationDescriptorImpl;
+import javax.media.jai.ParameterBlockJAI;
+import javax.media.jai.RenderedOp;
+import javax.media.jai.registry.RenderedRegistryMode;
+
+/**
+ * Describes the "FloodFill" operation.
+ * <p>
+ * This operation takes a single source image and identifies regions
+ * of connected pixels with uniform value, where value comparisons
+ * take into account a user-specified tolerance.
+ * <p>
+ * <b>Note: At present, this operator only deals with a single
+ * specified band.</b>
+ * <p>
+ * A pixel is connected to another pixel if a path can be defined
+ * between them that only passes through pixels with the same value,
+ * taking unit steps horizontally, vertically and, optionally,
+ * diagonally.
+ * <p>
+ * Each region that is identified in the source image is allocated
+ * a unique integer ID. The product of the operation is an image
+ * with data of TYPE_INT, where each pixel has its region ID as its
+ * value. A {@linkplain RegionData} object is also returned as 
+ * a property of the output image.  It can be retrieved by calling
+ * the {@code getProperty} method on this operator with "regiondata"
+ * as the property name (this name can also be referred to with
+ * {@linkplain FloodFillDescriptor#REGION_DATA_PROPERTY}).
+ * <p>
+ *
+ * <b>Parameters</b>
+ * <table border="1">
+ * <tr align="right">
+ * <td>Name</td><td>Type</td><td>Default value</td>
+ * </tr>
+ * <tr align="right">
+ * <td>band</td><td>int</td><td>0</td>
+ * </tr>
+ * <tr align="right">
+ * <td>tolerance</td><td>double</td><td>0d</td>
+ * </tr>
+ * <tr align="right">
+ * <td>diagonal</td><td>boolean</td><td>false</td>
+ * </tr>
+ * </table>
+ * 
+ * @author Michael Bedward
+ * @since 1.0
+ * @source $URL: https://jai-tools.googlecode.com/svn/trunk/operator/floodfill/src/main/java/jaitools/media/jai/floodfill/FloodFillDescriptor.java $
+ * @version $Id: FloodFillDescriptor.java 531 2009-08-06 12:27:52Z michael.bedward $
+ */
+public class FloodFillDescriptor extends OperationDescriptorImpl {
+
+    /**
+     * The propoerty name to retrieve the {@linkplain RegionData}
+     * object which holds summary data for regions identified in
+     * the source image and depicted in the destination image
+     */
+    public static final String REGION_DATA_PROPERTY = "regiondata";
+
+    static final int BAND_ARG_INDEX = 0;
+    static final int TOLERANCE_ARG_INDEX = 1;
+    static final int DIAGONAL_ARG_INDEX = 2;
+
+    private static final String[] paramNames =
+        {"band", "tolerance", "diagonal"};
+
+    private static final Class[] paramClasses =
+        {Integer.class, Double.class, Boolean.class};
+
+    private static final Object[] paramDefaults =
+        {Integer.valueOf(0), Double.valueOf(0d), Boolean.TRUE};
+
+    /** Constructor. */
+    public FloodFillDescriptor() {
+        super(new String[][]{
+                    {"GlobalName", "FloodFill"},
+                    {"LocalName", "FloodFill"},
+                    {"Vendor", "jaitools"},
+                    {"Description", "Identifies sufficiently uniform regions in a source image"},
+                    {"DocURL", "http://code.google.com/p/jai-tools/"},
+                    {"Version", "0.0.1"},
+                    {"arg0Desc", "band (int) - the band to floodfill"},
+                    {"arg1Desc", "tolerance (double) - tolerance for pixel value comparison"},
+                    {"arg2Desc", "diagonal (boolean) - true to include diagonal neighbours;" +
+                             "false for only orthogonal neighbours"}
+                },
+
+                new String[]{RenderedRegistryMode.MODE_NAME},   // supported modes
+                
+                1,                                              // number of sources
+                
+                paramNames,
+                paramClasses,
+                paramDefaults,
+                    
+                null                                            // valid values (none defined)
+                );
+
+
+    }
+
+    @Override
+    public boolean arePropertiesSupported() {
+        return true;
+    }
+
+    /**
+     * Convenience method which constructs a {@link ParameterBlockJAI} and
+     * invokes {@code JAI.create("floodfill", params) }.
+     * If an ImageLayout object is included in the RenderingHints passed to
+     * this method, any specification of the SampleModel for the destination
+     * image will be overridden such that the destination will always be
+     * TYPE_INT.
+     *
+     * @param source0 the source image
+     * @param band the band to process
+     * @param tolerance tolerance for pixel value comparisons
+     * @param diagonal true to include diagonal connections; false for only
+     * orthogonal connections
+     * @param hints rendering hints (may be null)
+     * @return the RenderedOp destination
+     */
+    public static RenderedOp create(
+            RenderedImage source0,
+            int band,
+            double tolerance,
+            boolean diagonal,
+            RenderingHints hints) {
+        ParameterBlockJAI pb =
+                new ParameterBlockJAI("FloodFill",
+                RenderedRegistryMode.MODE_NAME);
+
+        pb.setSource("source0", source0);
+        pb.setParameter(paramNames[BAND_ARG_INDEX], band);
+        pb.setParameter(paramNames[TOLERANCE_ARG_INDEX], tolerance);
+        pb.setParameter(paramNames[DIAGONAL_ARG_INDEX], diagonal);
+
+        return JAI.create("FloodFill", pb, hints);
+    }
+}
+
---
+++ branches/new-ops/floodfill/src/main/java/jaitools/media/jai/floodfill/FloodFillSpi.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2009 Michael Bedward
+ * 
+ * This file is part of jai-tools.
+ *
+ * jai-tools is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the 
+ * License, or (at your option) any later version.
+ *
+ * jai-tools is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public 
+ * License along with jai-tools.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
+package jaitools.media.jai.floodfill;
+
+import java.awt.image.renderable.RenderedImageFactory;
+import javax.media.jai.OperationDescriptor;
+import javax.media.jai.OperationRegistry;
+import javax.media.jai.OperationRegistrySpi;
+import javax.media.jai.registry.RenderedRegistryMode;
+
+/**
+ * OperationRegistrySpi implementation to register the "FloodFill"
+ * operation and its associated image factories.
+ *
+ * @author Michael Bedward
+ * @since 1.0
+ * @source $URL: $
+ * @version $Id: $
+ */
+public class FloodFillSpi implements OperationRegistrySpi {
+
+    /** The name of the product to which these operations belong. */
+    private String productName = "jaitools.media.jai.floodfill";
+ 
+    /** Default constructor. */
+    public FloodFillSpi() {}
+
+    /**
+     * Registers the FloodFill operation and its
+     * associated image factories across all supported operation modes.
+     *
+     * @param registry The registry with which to register the operations
+     * and their factories.
+     */
+    public void updateRegistry(OperationRegistry registry) {
+        OperationDescriptor op = new FloodFillDescriptor();
+        registry.registerDescriptor(op);
+        String descName = op.getName();
+        
+        RenderedImageFactory rif = new FloodFillRIF();
+
+        registry.registerFactory(RenderedRegistryMode.MODE_NAME,
+                                 descName,
+                                 productName,
+                                 rif);
+
+    }
+}
