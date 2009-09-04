ec0d08912073f9f0e4179e7b82340aa0c1333430
michael.bedward <michael.bedward@07dc4d45-bd82-665a-fa80-c0abab6b2a50>
2009-09-04 04:30:40 +0000

    removed all edge logic from the flood filler class
    git-svn-id: https://jaitools.googlecode.com/svn/trunk@547 07dc4d45-bd82-665a-fa80-c0abab6b2a50

--- operator/regionalize/src/main/java/jaitools/media/jai/regionalize/FloodFiller.java
+++ operator/regionalize/src/main/java/jaitools/media/jai/regionalize/FloodFiller.java
@@ -2,17 +2,17 @@
  * Copyright 2009 Michael Bedward
  * 
  * This file is part of jai-tools.
-
+ *
  * jai-tools is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation, either version 3 of the 
  * License, or (at your option) any later version.
-
+ *
  * jai-tools is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
-
+ *
  * You should have received a copy of the GNU Lesser General Public 
  * License along with jai-tools.  If not, see <http://www.gnu.org/licenses/>.
  * 
@@ -21,13 +21,10 @@
 package jaitools.media.jai.regionalize;
 
 import jaitools.numeric.DoubleComparison;
-import java.awt.Point;
 import java.awt.Rectangle;
 import java.awt.image.RenderedImage;
 import java.awt.image.WritableRaster;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.ListIterator;
 import java.util.Queue;
 import java.util.LinkedList;
 import java.util.List;
@@ -38,8 +35,7 @@ import javax.media.jai.iterator.WritableRandomIter;
 /**
  * Implements a flood-fill algorithm to work with Rasters.
  * <p>
- * The code is adapted (pretty much out of all recognition) from an
- * algorithm published in C# by J. Dunlap at:
+ * The code is (very highly) adapted from an algorithm published in C# by J. Dunlap at:
  * <pre>   http://www.codeproject.com/KB/GDI-plus/queuelinearfloodfill.aspx</pre>
  * which was subsequently ported to Java by Owen Kaluza.
  * <b>Any bugs in the present code are not their fault</b>.
@@ -55,17 +51,12 @@ import javax.media.jai.iterator.WritableRandomIter;
 public class FloodFiller {
 
     private RandomIter srcIter;
-    private int minSrcX;
-    private int maxSrcX;
-    private int minSrcY;
-    private int maxSrcY;
     
     private WritableRandomIter destIter;
     private int minDestX;
     private int maxDestX;
     private int minDestY;
     private int maxDestY;
-    private Point destTilePos;
 
     private int fillValue;
     private double tolerance;
@@ -76,30 +67,6 @@ public class FloodFiller {
     private Queue<ScanSegment> segmentsPending;
     private List<ScanSegment> segmentsFilled;
     private ScanSegment lastSegmentChecked;
-
-    private static class EdgePixel implements Comparable<EdgePixel> {
-        Point pos;
-        int fillValue;
-        double refValue;
-        private boolean isCorner = false;
-
-        public int compareTo(EdgePixel o) {
-            Integer i = Integer.valueOf(fillValue);
-            int comp = i.compareTo(o.fillValue);
-            if (comp == 0) {
-                i = Integer.valueOf(pos.y);
-                comp = i.compareTo(o.pos.y);
-                if (comp == 0) {
-                    i = Integer.valueOf(pos.x);
-                    comp = i.compareTo(o.pos.x);
-                }
-            }
-            return comp;
-        }
-    }
-
-    private List<EdgePixel> edgePixels;
-
 
     /**
      * Create a FloodFiller to work with the given source and destination rasters
@@ -114,16 +81,9 @@ public class FloodFiller {
 
         srcIter = RandomIterFactory.create(src, null);
 
-        minSrcX = src.getMinX();
-        maxSrcX = minSrcX + src.getWidth() - 1;
-        minSrcY = src.getMinY();
-        maxSrcY = minSrcY + src.getHeight() - 1;
-
         this.band = band;
         this.tolerance = tolerance;
         this.diagonal = diagonal;
-
-        edgePixels = new ArrayList<EdgePixel>();
     }
 
     /**
@@ -136,75 +96,6 @@ public class FloodFiller {
         maxDestX = bounds.x + bounds.width - 1;
         maxDestY = bounds.y + bounds.height - 1;
     }
-
-    /**
-     * If there were regions in a previous raster that hit the right or
-     * bottom raster edge they may continue onto this raster. This method
-     * checks for this and returns any part regions that are identified.
-     *
-     * @return a List of (part) Regions
-     */
-    public List<WorkingRegion> getCarryOverRegions() {
-        List<WorkingRegion> regions = new ArrayList<WorkingRegion>();
-        if (edgePixels.isEmpty()) return regions;
-
-        Collections.sort(edgePixels);
-        List<EdgePixel> cachedPixels = new ArrayList<EdgePixel>();
-
-        List<ScanSegment> segments;
-        while (!edgePixels.isEmpty()) {
-            EdgePixel ep = edgePixels.remove(0);
-            
-            // if this was a corner pixel we want to keep it for possible use
-            // with another adjacent raster
-            if (ep.isCorner) cachedPixels.add(ep);
-
-            boolean fill = false;
-
-            if (ep.pos.x == minDestX - 1 && ep.pos.y >= minDestY && ep.pos.y <= maxDestY) {
-                /* This edge pixel is horizontally adjacent to the current raster */
-                doFill(minDestX, ep.pos.y, ep.fillValue, ep.refValue);
-                fill = true;
-
-            } else if (ep.pos.y == minDestY - 1 && ep.pos.x >= minDestX && ep.pos.x <= maxDestX) {
-                /* This edge pixel is vertically adjacent to the current raster */
-                doFill(ep.pos.x, minDestY, ep.fillValue, ep.refValue);
-                fill = true;
-
-            } else {
-                /* Not adjacent to the current raster */
-                cachedPixels.add(ep);
-            }
-
-            if (fill) {
-                /*
-                 * Create a new (part) region and cull any unprocessed edge pixels
-                 * that also belong to it (corner pixels are transferred to the
-                 * cachedPixels list rather than being discarded)
-                 */
-                regions.add(new WorkingRegion(ep.fillValue, ep.refValue, segmentsFilled));
-
-                ListIterator<EdgePixel> iter = edgePixels.listIterator();
-                while (iter.hasNext()) {
-                    EdgePixel ep2 = iter.next();
-                    if (ep2.fillValue == ep.fillValue) {
-                        for (ScanSegment seg : segmentsFilled) {
-                            if ((ep2.pos.y == seg.y && ep2.pos.x == seg.startX - 1) ||
-                                (ep2.pos.y == seg.y - 1 && ep2.pos.x >= seg.startX && ep2.pos.x <= seg.endX)) {
-                                iter.remove();
-                                if (ep2.isCorner) cachedPixels.add(ep2);
-                                break;
-                            }
-                        }
-                    }
-                }
-            }
-        }
-
-        edgePixels.addAll(cachedPixels);
-        return regions;
-    }
-
 
     /**
      * Fill the region connected to the specified start pixel.
@@ -224,19 +115,6 @@ public class FloodFiller {
             throw new RuntimeException("need to initialize destination iterator first");
         }
 
-        doFill(x, y, fillValue, refValue);
-        return new WorkingRegion(fillValue, refValue, segmentsFilled);
-    }
-
-    /**
-     * Does the flood-fill for the {@linkplain #fill} and {@linkplain #getCarryOverRegions}
-     * methods
-     * @param x start pixel x coordinate
-     * @param y start pixel y coordinate
-     * @param fillValue the value to write to the destination image for this region
-     * @param refValue the reference value for this region
-     */
-    private void doFill(int x, int y, int fillValue, double refValue) {
         this.fillValue = fillValue;
         this.refValue = refValue;
 
@@ -275,6 +153,8 @@ public class FloodFiller {
                 }
             }
         }
+
+        return new WorkingRegion(fillValue, refValue, segmentsFilled);
     }
 
 
@@ -330,23 +210,6 @@ public class FloodFiller {
         segment = new ScanSegment(left, right, y);
         segmentsFilled.add(segment);
         segmentsPending.offer(segment);
-
-        if (right == maxDestX || y == maxDestY) {
-            EdgePixel ep = new EdgePixel();
-            ep.refValue = refValue;
-            ep.fillValue = fillValue;
-
-            if (right == maxDestX) {
-                ep.pos = new Point(right, y);
-                if (y == maxDestY) {
-                    ep.isCorner = true;
-                }
-            } else {
-                ep.pos = new Point(left, y);
-            }
-
-            edgePixels.add(ep);
-        }
 
         return segment;
     }
