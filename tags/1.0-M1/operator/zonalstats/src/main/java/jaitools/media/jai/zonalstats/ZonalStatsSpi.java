e03033a796c7f5278b29f5d0557821a7e9c9d9a6
michael.bedward <michael.bedward@07dc4d45-bd82-665a-fa80-c0abab6b2a50>
2009-09-17 07:18:05 +0000

    create 1.0 milestone M1
    git-svn-id: https://jaitools.googlecode.com/svn@817 07dc4d45-bd82-665a-fa80-c0abab6b2a50

---
+++ tags/1.0-M1/operator/zonalstats/src/main/java/jaitools/media/jai/zonalstats/ZonalStatsSpi.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2009 Michael Bedward
+ *
+ * This file is part of jai-tools.
+ *
+ * jai-tools is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * jai-tools is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with jai-tools.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package jaitools.media.jai.zonalstats;
+
+import java.awt.image.renderable.RenderedImageFactory;
+import javax.media.jai.OperationDescriptor;
+import javax.media.jai.OperationRegistry;
+import javax.media.jai.OperationRegistrySpi;
+import javax.media.jai.registry.RenderedRegistryMode;
+
+/**
+ * OperationRegistrySpi implementation to register the "ZonalStats"
+ * operation and its associated image factories.
+ *
+ * @author Michael Bedward
+ * @since 1.0
+ * @source $URL$
+ * @version $Id$
+ */
+public class ZonalStatsSpi implements OperationRegistrySpi {
+
+    /** The name of the product to which these operations belong. */
+    private String productName = "jaitools.media.jai";
+ 
+    /** Default constructor. */
+    public ZonalStatsSpi() {}
+
+    /**
+     * Registers the ZonalStats operation
+     *
+     * @param registry The registry with which to register the operation
+     */
+    public void updateRegistry(OperationRegistry registry) {
+        OperationDescriptor op = new ZonalStatsDescriptor();
+        registry.registerDescriptor(op);
+        String descName = op.getName();
+        
+        RenderedImageFactory rif = new ZonalStatsRIF();
+
+        registry.registerFactory(RenderedRegistryMode.MODE_NAME,
+                                 descName,
+                                 productName,
+                                 rif);
+
+    }
+}
