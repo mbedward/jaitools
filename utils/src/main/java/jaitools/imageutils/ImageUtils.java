e62e05019759320b6e1c89402a023c5d1c1f9806
michael.bedward <michael.bedward@07dc4d45-bd82-665a-fa80-c0abab6b2a50>
2009-11-13 07:33:01 +0000

    Tidied up inconsistencies in ImageUtils methods; deprecated createDoubleImage methods and added createConstantImage; deprectated existing createDisplayImage and added a new version that takes a colour lookup table as an arg; added simple colour ramp generating methods
    git-svn-id: https://jaitools.googlecode.com/svn/trunk@1040 07dc4d45-bd82-665a-fa80-c0abab6b2a50

--- utils/src/main/java/jaitools/imageutils/ImageUtils.java
+++ utils/src/main/java/jaitools/imageutils/ImageUtils.java
@@ -25,12 +25,16 @@ import java.awt.Color;
 import java.awt.image.RenderedImage;
 import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 import java.util.SortedSet;
+import java.util.TreeMap;
 import javax.media.jai.JAI;
 import javax.media.jai.LookupTableJAI;
 import javax.media.jai.ParameterBlockJAI;
 import javax.media.jai.RenderedOp;
 import javax.media.jai.TiledImage;
+import javax.media.jai.iterator.RectIterFactory;
+import javax.media.jai.iterator.WritableRectIter;
 
 /**
  * Static helper functions for common image tasks.
@@ -48,6 +52,9 @@ public class ImageUtils {
      * @param width image width in pixels
      * @param height image height in pixels
      * @return a new TiledImage object
+     *
+     * @deprecated This method will be removed in version 1.1.
+     *             Please use {@linkplain #createConstantImage(int, int, java.lang.Number)}
      */
     public static TiledImage createDoubleImage(int width, int height) {
         return createDoubleImage(width, height, new double[] {0});
@@ -60,6 +67,9 @@ public class ImageUtils {
      * @param height image height in pixels
      * @param numBands number of image bands (must be >= 1)
      * @return a new TiledImage object
+     *
+     * @deprecated This method will be removed in version 1.1.
+     *             Please use {@linkplain #createConstantImage(int, int, java.lang.Number)}
      */
     public static TiledImage createDoubleImage(int width, int height, int numBands) {
         if (numBands < 1) {
@@ -80,6 +90,9 @@ public class ImageUtils {
      * @param height image height in pixels
      * @param values array of double values (must contain at least one element)
      * @return a new TiledImage object
+     *
+     * @deprecated This method will be removed in version 1.1.
+     *             Please use {@linkplain #createConstantImage(int width, int height, Number[] values)}
      */
     public static TiledImage createDoubleImage(int width, int height, double[] values) {
         if (values == null || values.length < 1) {
@@ -101,8 +114,132 @@ public class ImageUtils {
     }
 
     /**
+     * Creates a new TiledImage object with a single band of constant value.
+     * The data type of the image corresponds to the class of {@code value}.
+     *
+     * @param width image width in pixels
+     *
+     * @param height image height in pixels
+     *
+     * @param value the constant value to fill the image
+     *
+     * @return a new TiledImage object
+     */
+    public static TiledImage createConstantImage(int width, int height, Number value) {
+        return createConstantImage(width, height, new Number[] {value});
+    }
+
+    /**
+     * Creates a new TiledImage object with one or more bands of constant value.
+     * The number of bands in the output image corresponds to the length of
+     * the input values array and the data type of the image corresponds to the
+     * {@code Number} class used.
+     *
+     * @param width image width in pixels
+     *
+     * @param height image height in pixels
+     *
+     * @param values array of values (must contain at least one element)
+     *
+     * @return a new TiledImage object
+     */
+    public static TiledImage createConstantImage(int width, int height, Number[] values) {
+        if (values == null || values.length < 1) {
+            throw new IllegalArgumentException("values array must contain at least 1 value");
+        }
+
+        ParameterBlockJAI pb = new ParameterBlockJAI("constant");
+        pb.setParameter("width", (float)width);
+        pb.setParameter("height", (float)height);
+
+        Number[] typedValues = null;
+        if (values[0] instanceof Double) {
+            typedValues = new Double[values.length];
+        } else if (values[0] instanceof Float) {
+            typedValues = new Float[values.length];
+        } else if (values[0] instanceof Integer) {
+            typedValues = new Integer[values.length];
+        } else if (values[0] instanceof Short) {
+            typedValues = new Short[values.length];
+        } else if (values[0] instanceof Byte) {
+            typedValues = new Byte[values.length];
+        } else {
+            throw new UnsupportedOperationException("Unsupported data type: " +
+                    values[0].getClass().getName());
+        }
+
+        for (int i = 0; i < values.length; i++) {
+            typedValues[i] = values[i];
+        }
+        
+        pb.setParameter("bandValues", typedValues);
+
+        RenderedOp op = JAI.create("constant", pb);
+        return new TiledImage(op, false);
+    }
+    
+    /**
+     * Create a set of colours using a simple colour ramp algorithm in the HSB colour space.
+     *
+     * @param numColours number of colours required
+     *
+     * @return an array of colours sampled from the HSB space.
+     */
+    public static Color[] createRampColours(int numColours) {
+        return createRampColours(numColours, 0.8f, 0.8f);
+    }
+
+    /**
+     * Create a set of colours using a simple colour ramp algorithm in the HSB colour space.
+     *
+     * @param numColours number of colours required
+     *
+     * @param saturation the saturation of all colours (between 0 and 1)
+     *
+     * @param brightness the brightness of all colours (between 0 and 1)
+     *
+     * @return an array of colours sampled from the HSB space between the start and end hues
+     */
+    public static Color[] createRampColours(int numColours, float saturation, float brightness) {
+        return createRampColours(numColours, 0.0f, 1.0f, saturation, brightness);
+    }
+
+    /**
+     * Create a set of colours using a simple colour ramp algorithm in the HSB colour space.
+     * All float arguments should be values between 0 and 1.
+     *
+     * @param numColours number of colours required
+     *
+     * @param startHue the starting hue
+     *
+     * @param endHue the ending hue
+     *
+     * @param saturation the saturation of all colours
+     *
+     * @param brightness the brightness of all colours
+     *
+     * @return an array of colours sampled from the HSB space between the start and end hues
+     */
+    public static Color[] createRampColours(int numColours, float startHue, float endHue,
+            float saturation, float brightness) {
+
+        Color[] colors = new Color[numColours];
+
+        final float increment = numColours > 1 ? (endHue - startHue) / (float)(numColours - 1) : 0f;
+        float hue = startHue;
+        for (int i = 0; i < numColours; i++) {
+            int rgb = Color.HSBtoRGB(hue, saturation, brightness);
+            colors[i] = new Color(rgb);
+            hue += increment;
+        }
+
+        return colors;
+    }
+
+    /**
      * A helper method that creates a 3-band colour image, using a colour ramp,
-     * for a data image with integral data type and pixel values between 0 and ncol.
+     * for a data image with integral data type and pixel values between 1 and
+     * {@code ncol-1}.
      * <p>
      * This method is only preliminary and, presently, very limited in the
      * input images that it will deal with.
@@ -113,6 +250,11 @@ public class ImageUtils {
      * @param ncol number of colours to be created using a colour ramp
      *
      * @return a three band image with an RGB ColorModel
+     * 
+     * @deprecated This method will be removed in version 1.1.
+     *             Please use {@linkplain #createDisplayImage(RenderedImage, Map)} instead.
+     *             Colour ramp colours can be generated with
+     *             {@linkplain #createRampColours(int)}.
      */
     public static RenderedImage createDisplayImage(RenderedImage dataImg, int ncol) {
 
@@ -136,7 +278,92 @@ public class ImageUtils {
     }
 
     /**
-     * Get the bands of a multi-band image as a List of single-band images.
+     * Creates a proxy RGB display image for the given data image. The data image should be
+     * of integral data type. Only the first band of multi-band images will be used.
+     *
+     * @param dataImg the data image
+     *
+     * @param colourTable a lookup table giving colours for each data image value
+     *
+     * @return a new RGB image
+     */
+    public static RenderedImage createDisplayImage(RenderedImage dataImg, Map<Integer, Color> colourTable) {
+
+        if (colourTable.size() > 256) {
+            throw new IllegalArgumentException("Number of colours can't be more than 256");
+        }
+
+        Integer maxKey = null;
+        Integer minKey = null;
+        for (Integer key : colourTable.keySet()) {
+            if (minKey == null) {
+                minKey = maxKey = key;
+
+            } else if (key < minKey) {
+                minKey = key;
+            } else if (key > maxKey) {
+                maxKey = key;
+            }
+        }
+
+        ParameterBlockJAI pb = null;
+        RenderedImage lookupImg = dataImg;
+        byte[][] lookup = null;
+        int offset = 0;
+
+        if (minKey < 0 || maxKey > 255) {
+            lookupImg = createConstantImage(dataImg.getWidth(), dataImg.getHeight(), Integer.valueOf(0));
+
+            TreeMap<Integer, Integer> keyTable = CollectionFactory.newTreeMap();
+            int k = 0;
+            for (Integer key : colourTable.keySet()) {
+                keyTable.put(key, k++);
+            }
+
+            WritableRectIter iter = RectIterFactory.createWritable((TiledImage)lookupImg, null);
+            do {
+                do {
+                    do {
+                        iter.setSample( keyTable.get(iter.getSample()) );
+                    } while (!iter.nextPixelDone());
+                    iter.startPixels();
+                } while (!iter.nextLineDone());
+                iter.startLines();
+            } while (!iter.nextBandDone());
+
+            lookup = new byte[3][colourTable.size()];
+            for (Integer key : keyTable.keySet()) {
+                int index = keyTable.get(key);
+                int colour = colourTable.get(key).getRGB();
+                lookup[0][index] = (byte) ((colour & 0x00ff0000) >> 16);
+                lookup[1][index] = (byte) ((colour & 0x0000ff00) >> 8);
+                lookup[2][index] = (byte) (colour & 0x000000ff);
+            }
+
+        } else {
+            lookup = new byte[3][maxKey - minKey + 1];
+            offset = minKey;
+
+            for (Integer key : colourTable.keySet()) {
+                int colour = colourTable.get(key).getRGB();
+                lookup[0][key - offset] = (byte) ((colour & 0x00ff0000) >> 16);
+                lookup[1][key - offset] = (byte) ((colour & 0x0000ff00) >> 8);
+                lookup[2][key - offset] = (byte) (colour & 0x000000ff);
+            }
+        }
+
+        pb = new ParameterBlockJAI("Lookup");
+        pb.setSource("source0", lookupImg);
+        pb.setParameter("table", new LookupTableJAI(lookup, offset));
+        RenderedOp displayImg = JAI.create("Lookup", pb);
+        
+        return displayImg;
+    }
+
+    /**
+     * Get the bands of a multi-band image as a list of single-band images. This can
+     * be used, for example, to separate the result image returned by the KernelStats
+     * operator into separate result images.
      *
      * @param img the multi-band image
      * @return a List of new single-band images
@@ -159,7 +386,9 @@ public class ImageUtils {
     }
 
     /**
-     * Get the specified bands of a multi-band image as a List of single-band images.
+     * Get the specified bands of a multi-band image as a list of single-band images. This can
+     * be used, for example, to separate the result image returned by the KernelStats
+     * operator into separate result images.
      *
      * @param img the multi-band image
      * @param bandIndices a Collection of Integer indices in the range 0 <= i < number of bands
