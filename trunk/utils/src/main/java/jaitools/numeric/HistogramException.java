1e0cff82949eb504b97bf4f29c62be95e49c5688
michael.bedward <michael.bedward@07dc4d45-bd82-665a-fa80-c0abab6b2a50>
2009-12-14 11:10:32 +0000

    Added Histogram classes. Added copy ctor to Range.
    git-svn-id: https://jaitools.googlecode.com/svn@1061 07dc4d45-bd82-665a-fa80-c0abab6b2a50

---
+++ trunk/utils/src/main/java/jaitools/numeric/HistogramException.java
@@ -0,0 +1,15 @@
+package jaitools.numeric;
+
+/**
+ * @author Michael Bedward
+ */
+class HistogramException extends IllegalArgumentException {
+
+    public HistogramException() {
+    }
+
+    public HistogramException(String msg) {
+        super(msg);
+    }
+
+}
