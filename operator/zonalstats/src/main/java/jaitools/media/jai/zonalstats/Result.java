7e0a972c6ce25033100c7cdc4350e5714d3ef1ba
michael.bedward@gmail.com <michael.bedward@gmail.com@07dc4d45-bd82-665a-fa80-c0abab6b2a50>
2010-11-10 10:18:43 +0000

    added javadocs
    git-svn-id: https://jaitools.googlecode.com/svn/trunk@1212 07dc4d45-bd82-665a-fa80-c0abab6b2a50

--- operator/zonalstats/src/main/java/jaitools/media/jai/zonalstats/Result.java
+++ operator/zonalstats/src/main/java/jaitools/media/jai/zonalstats/Result.java
@@ -28,6 +28,11 @@ import java.util.Collections;
 import java.util.List;
 
 /**
+ * Holds the result for a a given combination of image band / zone / statistic.
+ * See documentation for {@code ZonalStats} for examples of how to access 
+ * results.
+ * 
+ * @see ZonalStats
  * @author Michael Bedward
  */
 public class Result {
@@ -42,15 +47,25 @@ public class Result {
     private long numNaN;
     private long numNoData;
 
-    public long getNumNaN() {
-        return numNaN;
-    }
-
-    public long getNumNoData() {
-        return numNoData;
-    }
-
-    public Result(int imageBand, int zone, Statistic stat, List<Range> ranges, Double value, long numOffered, long numAccepted, long numNaN, long numNoData) {
+    /**
+     * Create a new {@code Result} object. This is intended for use by the
+     * {@code ZonalStats} class rather than client code.
+     * 
+     * @param imageBand data image band
+     * @param zone integer identifier of the zone in the zone image; 0 should be passed
+     *        when no zone image was used
+     * @param stat the statistic to which this result pertains
+     * @param ranges list of ranges (if any) used to filter data image values
+     * @param value the calculated value of the statistic
+     * @param numOffered number of data image values considered for inclusion
+     * @param numAccepted number of data image values actually used for calculating
+     *        this result
+     * @param numNaN number of NaN values read from the data image
+     * @param numNoData number of NoData values read from the data image
+     */
+    public Result(int imageBand, int zone, Statistic stat, List<Range> ranges, 
+                  Double value, long numOffered, long numAccepted, long numNaN, 
+                  long numNoData) {
         this.imageBand = imageBand;
         this.zone = zone;
         this.stat = stat;
@@ -62,32 +77,93 @@ public class Result {
         this.ranges = ranges;
     }
 
+    /**
+     * Get the ranges (if defined) that were used to filter data image
+     * values for inclusion in the calculation of this result.
+     * 
+     * @return ranges used to filter data image values
+     */
     public Collection<Range> getRanges() {
         return Collections.unmodifiableCollection(ranges);
     }
 
+    /**
+     * Get the index of the image band for which this result
+     * was calculated.
+     * 
+     * @return image band index
+     */
     public int getImageBand() {
         return imageBand;
     }
 
+    /**
+     * Get the integer ID of the image zone for which this result was
+     * calculated. If no zone image was used this method will
+     * return 0.
+     * 
+     * @return integer ID of the image zone.
+     */
     public int getZone() {
         return zone;
     }
 
+    /**
+     * Get the statistic that this result pertains to.
+     * 
+     * @return the statistic
+     */
     public Statistic getStatistic() {
         return stat;
     }
 
+    /**
+     * Get the calculated value of the statistic.
+     * 
+     * @return value of the statistic
+     */
     public Double getValue() {
         return value;
     }
 
+    /**
+     * Get the number of values that were accepted, ie. sampled from the image
+     * and included in the calculation of this result.
+     * 
+     * @return the number of values accepted
+     */
     public long getNumAccepted() {
         return numAccepted;
     }
 
+    /**
+     * Get the number of values that were offered, ie. sampled from the image
+     * and considered for inclusion in this result.
+     * 
+     * @return number of values offered
+     */
     public long getNumOffered() {
         return numOffered;
+    }
+
+    /**
+     * Get the number of NaN values that were sampled from the image when
+     * calculating this result.
+     * 
+     * @return number of NaN values sampled
+     */
+    public long getNumNaN() {
+        return numNaN;
+    }
+
+    /**
+     * Get the number of NoData values that were sampled from the image when
+     * calculating this result.
+     * 
+     * @return number of NoData values sampled
+     */
+    public long getNumNoData() {
+        return numNoData;
     }
 
     @Override
