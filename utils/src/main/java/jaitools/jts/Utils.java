8c792418377423bc575d4f207f19520bddcfd9bb
michael.bedward <michael.bedward@07dc4d45-bd82-665a-fa80-c0abab6b2a50>
2011-01-18 02:11:11 +0000

    Issue 69: GeometryFactory getter and precision model methods; reformatted source
    git-svn-id: https://jaitools.googlecode.com/svn/trunk@1295 07dc4d45-bd82-665a-fa80-c0abab6b2a50

--- utils/src/main/java/jaitools/jts/Utils.java
+++ utils/src/main/java/jaitools/jts/Utils.java
@@ -1,3 +1,23 @@
+/*
+ * Copyright 2010-2011 Michael Bedward
+ * 
+ * This file is part of jai-tools.
+ *
+ * jai-tools is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the 
+ * License, or (at your option) any later version.
+ *
+ * jai-tools is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public 
+ * License along with jai-tools.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
 package jaitools.jts;
 
 import java.util.ArrayList;
@@ -13,141 +33,177 @@ import com.vividsolutions.jts.geom.LineString;
 import com.vividsolutions.jts.geom.LinearRing;
 import com.vividsolutions.jts.geom.Polygon;
 import com.vividsolutions.jts.geom.PrecisionModel;
+
 /**
- * Helper class for utilities based on JTS classes 
+ * A helper class with methods to work with JTS geometry objects.
+ * 
  * @author Simone Giannecchini, GeoSolutions SAS
- *
+ * @author Michael Bedward
+ * @since 1.1
+ * @source $URL$
+ * @version $Id$
  */
 public class Utils {
 
-	/** Geometry factory used to create LineStrings */
-	public final static GeometryFactory GEOMETRY_FACTORY= new GeometryFactory(new PrecisionModel(100));
-	
-	private Utils() {
-	}
-	
-	/**
-	 * Removes collinear points from the provided linestring.
-	 * 
-	 * @param ls the {@link LineString} to be simplified.
-	 * @return a new version of the provided {@link LineString} with collinear points removed.
-	 */
-	public static LineString removeCollinearVertices(final LineString ls){
-		if(ls==null)
-			throw new NullPointerException("The provided linestring is null");
-	    final int N = ls.getNumPoints();
-	    final boolean isLinearRing = ls instanceof LinearRing;
-	    
-	    List<Coordinate> retain = new ArrayList<Coordinate>();
-	    retain.add(ls.getCoordinateN(0));
-	    
-	    int i0 = 0, i1 = 1, i2 = 2;
-	    Coordinate firstCoord=ls.getCoordinateN(i0);
-	    Coordinate midCoord;
-	    Coordinate lastCoord;
-	    while (i2 < N) {
+    private static GeometryFactory geomFactory;
 
-	    	midCoord=ls.getCoordinateN(i1);
-	    	lastCoord=ls.getCoordinateN(i2);
-	    	
-	    	// perform a redundancy check based on 
-	        double dx1 = midCoord.x - firstCoord.x;
-	        double dy1 = midCoord.y - firstCoord.y;
-	        double dx2 = lastCoord.x - midCoord.x;
-	        double dy2 = lastCoord.y - midCoord.y;
-	        
-	        boolean redundant = false;
-	        if(dzero(dx1)) {
-	        	if(dzero(dx2) && Math.signum(dy1) == Math.signum(dy2)) {
-	        		redundant = true;
-	        	}
-	        } else {
-	        	if(!dzero(dx2)) {
-	        		if(dy1 / dx1 == dy2 / dx2 && Math.signum(dx1) == Math.signum(dx2)) {
-	        			redundant = true;
-	        		}
-	        	}
-	        }
+    private Utils() {
+    }
+    
+    /**
+     * Gets the cached {@code GeometryFactory} instance.
+     * 
+     * @return the geometry factory
+     */
+    public static GeometryFactory getGeometryFactory() {
+        if (geomFactory == null) {
+            geomFactory = new GeometryFactory();
+        }
+        return geomFactory;
+    }
+    
+    /**
+     * Sets the precision to be used by the cached {@code GeometryFactory}.
+     * 
+     * @param pm a precision model or {@code null} for default precision
+     * 
+     * @return the (possibly new) cached {@code GeometryFactory} instance
+     * 
+     * @see PrecisionModel
+     */
+    public static GeometryFactory setPrecision(PrecisionModel pm) {
+        PrecisionModel model = pm == null ? new PrecisionModel() : pm;
+        geomFactory = new GeometryFactory(model);
+        return geomFactory;
+    }
 
-	    	
-	        // Add only if not redundant
-	        if (!redundant) {
-	        	// add midcoord and change head
-	            retain.add(midCoord);
-	            i0=i1;
-	            firstCoord=ls.getCoordinateN(i0);
-	        }
-	        i1++; i2++;	    	
-	    }
-	    retain.add(ls.getCoordinateN(N-1));
+    /**
+     * Removes collinear points from the provided linestring.
+     * 
+     * @param ls the {@link LineString} to be simplified.
+     * @return a new version of the provided {@link LineString} with collinear points removed.
+     */
+    public static LineString removeCollinearVertices(final LineString ls) {
+        if (ls == null) {
+            throw new NullPointerException("The provided linestring is null");
+        }
+        final int N = ls.getNumPoints();
+        final boolean isLinearRing = ls instanceof LinearRing;
 
-	    //
-	    // Return value
-	    //
-	    final int size=retain.size();
-	    // nothing changed?
-	    if(size==N){
-	    	// free everything and return original
-	    	retain.clear();
-	    	return ls;
-	    }
-	    return isLinearRing?
-	    		ls.getFactory().createLinearRing(retain.toArray(new Coordinate[size])):
-	    			ls.getFactory().createLineString(retain.toArray(new Coordinate[size]));
-	}
-	
-	/**
-	 * Removes collinear vertices from the provided {@link Polygon}.
-	 * @param polygon the instance of a {@link Polygon} to remove collinear vertices from.
-	 * @return a new instance of the provided  {@link Polygon} without collinear vertices.
-	 */
-	public static Polygon removeCollinearVertices(final Polygon polygon) {
-		if(polygon==null)
-			throw new NullPointerException("The provided Polygon is null");	
-		// reuse existing factory		
+        List<Coordinate> retain = new ArrayList<Coordinate>();
+        retain.add(ls.getCoordinateN(0));
+
+        int i0 = 0, i1 = 1, i2 = 2;
+        Coordinate firstCoord = ls.getCoordinateN(i0);
+        Coordinate midCoord;
+        Coordinate lastCoord;
+        while (i2 < N) {
+
+            midCoord = ls.getCoordinateN(i1);
+            lastCoord = ls.getCoordinateN(i2);
+
+            // perform a redundancy check based on 
+            double dx1 = midCoord.x - firstCoord.x;
+            double dy1 = midCoord.y - firstCoord.y;
+            double dx2 = lastCoord.x - midCoord.x;
+            double dy2 = lastCoord.y - midCoord.y;
+
+            boolean redundant = false;
+            if (dzero(dx1)) {
+                if (dzero(dx2) && Math.signum(dy1) == Math.signum(dy2)) {
+                    redundant = true;
+                }
+            } else {
+                if (!dzero(dx2)) {
+                    if (dy1 / dx1 == dy2 / dx2 && Math.signum(dx1) == Math.signum(dx2)) {
+                        redundant = true;
+                    }
+                }
+            }
+
+
+            // Add only if not redundant
+            if (!redundant) {
+                // add midcoord and change head
+                retain.add(midCoord);
+                i0 = i1;
+                firstCoord = ls.getCoordinateN(i0);
+            }
+            i1++;
+            i2++;
+        }
+        retain.add(ls.getCoordinateN(N - 1));
+
+        //
+        // Return value
+        //
+        final int size = retain.size();
+        // nothing changed?
+        if (size == N) {
+            // free everything and return original
+            retain.clear();
+            return ls;
+        }
+        return isLinearRing
+                ? ls.getFactory().createLinearRing(retain.toArray(new Coordinate[size]))
+                : ls.getFactory().createLineString(retain.toArray(new Coordinate[size]));
+    }
+
+    /**
+     * Removes collinear vertices from the provided {@link Polygon}.
+     * @param polygon the instance of a {@link Polygon} to remove collinear vertices from.
+     * @return a new instance of the provided  {@link Polygon} without collinear vertices.
+     */
+    public static Polygon removeCollinearVertices(final Polygon polygon) {
+        if (polygon == null) {
+            throw new NullPointerException("The provided Polygon is null");
+        }
+        // reuse existing factory		
         final GeometryFactory gf = polygon.getFactory();
 
         // work on the exterior ring
         LineString exterior = polygon.getExteriorRing();
         LineString shell = removeCollinearVertices(exterior);
-        if(shell == null || shell.isEmpty()) {
+        if (shell == null || shell.isEmpty()) {
             return null;
         }
 
         // work on the holes
         List<LineString> holes = new ArrayList<LineString>();
-        final int size=polygon.getNumInteriorRing();
-        for (int i = 0; i <size; i++) {
-        	LineString hole = polygon.getInteriorRingN(i);
+        final int size = polygon.getNumInteriorRing();
+        for (int i = 0; i < size; i++) {
+            LineString hole = polygon.getInteriorRingN(i);
             hole = removeCollinearVertices(hole);
-            if(hole != null && !hole.isEmpty()) {
+            if (hole != null && !hole.isEmpty()) {
                 holes.add(hole);
             }
         }
 
-        return gf.createPolygon((LinearRing)shell, (LinearRing[]) holes.toArray(new LinearRing[holes.size()]));
-	}
-	
-	/**
-	 * Removes collinear vertices from the provided {@link Geometry}.
-	 * 
-	 * <p>
-	 * For the moment this implementation only accepts, {@link Polygon}, {@link LinearRing} and {@link LineString}.
-	 * It return <code>null</code> in case the geometry is not of these types. 
-	 * 
-	 * @todo implement submethods for {@link GeometryCollection} sublcaases.
-	 * @param g the instance of a {@link Geometry} to remove collinear vertices from.
-	 * @return a new instance of the provided  {@link Geometry} without collinear vertices.
-	 */
-	public static Geometry removeCollinearVertices(final Geometry g) {
-		if(g==null)
-			throw new NullPointerException("The provided Geometry is null");		
-		if(g instanceof LineString)
-			return removeCollinearVertices((LineString)g);
-		if(g instanceof Polygon)
-			return removeCollinearVertices((Polygon)g);			
-			 
-		throw new IllegalArgumentException("This method can work on LineString and Polygon.");
-	}
+        return gf.createPolygon((LinearRing) shell, (LinearRing[]) holes.toArray(new LinearRing[holes.size()]));
+    }
 
+    /**
+     * Removes collinear vertices from the provided {@link Geometry}.
+     * 
+     * <p>
+     * For the moment this implementation only accepts, {@link Polygon}, {@link LinearRing} and {@link LineString}.
+     * It return <code>null</code> in case the geometry is not of these types. 
+     * 
+     * @todo implement submethods for {@link GeometryCollection} sublcaases.
+     * @param g the instance of a {@link Geometry} to remove collinear vertices from.
+     * @return a new instance of the provided  {@link Geometry} without collinear vertices.
+     */
+    public static Geometry removeCollinearVertices(final Geometry g) {
+        if (g == null) {
+            throw new NullPointerException("The provided Geometry is null");
+        }
+        if (g instanceof LineString) {
+            return removeCollinearVertices((LineString) g);
+        }
+        if (g instanceof Polygon) {
+            return removeCollinearVertices((Polygon) g);
+        }
+
+        throw new IllegalArgumentException("This method can work on LineString and Polygon.");
+    }
 }
--- utils/src/test/java/jaitools/jts/UtilsTest.java
+++ utils/src/test/java/jaitools/jts/UtilsTest.java
@@ -1,15 +1,33 @@
-/**
+/*
+ * Copyright 2010-2011 Michael Bedward
+ * 
+ * This file is part of jai-tools.
+ *
+ * jai-tools is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the 
+ * License, or (at your option) any later version.
+ *
+ * jai-tools is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public 
+ * License along with jai-tools.  If not, see <http://www.gnu.org/licenses/>.
  * 
  */
 package jaitools.jts;
 
-import org.junit.Assert;
+import static org.junit.Assert.*;
 import org.junit.Test;
 
 import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.geom.GeometryFactory;
 import com.vividsolutions.jts.geom.LineString;
 import com.vividsolutions.jts.geom.LinearRing;
 import com.vividsolutions.jts.geom.Polygon;
+import com.vividsolutions.jts.geom.PrecisionModel;
 import com.vividsolutions.jts.io.ParseException;
 import com.vividsolutions.jts.io.WKTReader;
 
@@ -17,93 +35,111 @@ import com.vividsolutions.jts.io.WKTReader;
  * Tests for the JTS utility class.
  * 
  * @author Simone Giannecchini, GeoSolutions SAS
- *
+ * @author Michael Bedward
+ * @since 1.1
+ * @source $URL$
+ * @version $Id$
  */
-public class UtilsTest extends Assert {
 
-	private final static String LINESTRING= "LINESTRING(0 0, 1 0, 2 0, 5 0, 5 1, 5 2, 5 5, 6 5, 6 8, 6 9, 7 0)";
-	private final static String EXPECTED_LINESTRING= "LINESTRING(0 0, 5 0, 5 5, 6 5, 6 9, 7 0)";
-	
-	private final static String POLYGON="POLYGON((1 1, 2 1, 3 1, 4 1, 5 1, 5 2, 5 3, 5 4, 5 5, 4 5, 3 5, 2 5, 1 5, 1 4, 1 3, 1 2, 1 1),(2 2,2 3,3 3,3 2,2 2))";
-	private final static String EXPECTED_POLYGON="POLYGON((1 1,5 1,5 5,1 5,1 1),(2 2,2 3,3 3,3 2,2 2))";
-	
-	private final static String RING="LINEARRING(1 1, 2 1, 3 1, 4 1, 5 1, 5 2, 5 3, 5 4, 5 5, 4 5, 3 5, 2 5, 1 5, 1 4, 1 3, 1 2, 1 1)";
-	private final static String EXPECTED_RING="LINEARRING(1 1,5 1,5 5,1 5,1 1)";
-	
-	private final static String DEGENARATE_LS = "LINESTRING(1 1, 2 2, 1 1)";
-	
-	@Test
-	public void testLineString() throws ParseException {
-		final LineString ls= (LineString) new WKTReader().read(LINESTRING);
-		
-		// simplify
-		final LineString simplifiedLS= Utils.removeCollinearVertices(ls);
-		
-		// check expected
-		assertTrue(((LineString) new WKTReader().read(EXPECTED_LINESTRING)).equalsExact(simplifiedLS));
-	}
+public class UtilsTest {
 
-	@Test
-	public void testLinePolygon() throws ParseException {
-		final Polygon poly= (Polygon) new WKTReader().read(POLYGON);
-		
-		// simplify
-		final Polygon simplifiedPoly= Utils.removeCollinearVertices(poly);
-		
-		// check expected
-		assertTrue(((Polygon) new WKTReader().read(EXPECTED_POLYGON)).equalsExact(simplifiedPoly));
-	}
-	
-	@Test
-	public void testLineRing() throws ParseException {
-		final LinearRing ring= (LinearRing) new WKTReader().read(RING);
-		
-		// simplify
-		final LinearRing simplifiedring= (LinearRing) Utils.removeCollinearVertices(ring);
-		
-		// check expected
-		assertTrue(((LinearRing) new WKTReader().read(EXPECTED_RING)).equalsExact(simplifiedring));
-	}
-	
-	@Test
-	public void testDegenerate() throws ParseException {
-		final LineString original = (LineString) new WKTReader().read(DEGENARATE_LS);
-		
-		// simplify
-		final LineString simplified = (LineString) Utils.removeCollinearVertices(original);
-		
-		// check expected
-		assertTrue(simplified.equalsExact(original));
-	}
+    private final static String LINESTRING = "LINESTRING(0 0, 1 0, 2 0, 5 0, 5 1, 5 2, 5 5, 6 5, 6 8, 6 9, 7 0)";
+    private final static String EXPECTED_LINESTRING = "LINESTRING(0 0, 5 0, 5 5, 6 5, 6 9, 7 0)";
+    private final static String POLYGON = "POLYGON((1 1, 2 1, 3 1, 4 1, 5 1, 5 2, 5 3, 5 4, 5 5, 4 5, 3 5, 2 5, 1 5, 1 4, 1 3, 1 2, 1 1),(2 2,2 3,3 3,3 2,2 2))";
+    private final static String EXPECTED_POLYGON = "POLYGON((1 1,5 1,5 5,1 5,1 1),(2 2,2 3,3 3,3 2,2 2))";
+    private final static String RING = "LINEARRING(1 1, 2 1, 3 1, 4 1, 5 1, 5 2, 5 3, 5 4, 5 5, 4 5, 3 5, 2 5, 1 5, 1 4, 1 3, 1 2, 1 1)";
+    private final static String EXPECTED_RING = "LINEARRING(1 1,5 1,5 5,1 5,1 1)";
+    private final static String DEGENERATE_LS = "LINESTRING(1 1, 2 2, 1 1)";
 
-	
-	@Test
-	public void testGeometry() throws ParseException {
-		final Geometry ring= new WKTReader().read(RING);
-		
-		// simplify
-		final LinearRing simplifiedring=  (LinearRing) Utils.removeCollinearVertices(ring);
-		
-		// check expected
-		assertTrue(((LinearRing) new WKTReader().read(EXPECTED_RING)).equalsExact(simplifiedring));
-		
-		
-		final Geometry poly=  new WKTReader().read(POLYGON);
-		
-		// simplify
-		final Polygon simplifiedPoly= (Polygon) Utils.removeCollinearVertices(poly);
-		
-		// check expected
-		assertTrue(((Polygon) new WKTReader().read(EXPECTED_POLYGON)).equalsExact(simplifiedPoly));
-		
-		
-		final Geometry ls=  new WKTReader().read(LINESTRING);
-		
-		// simplify
-		final LineString simplifiedLS= (LineString) Utils.removeCollinearVertices(ls);
-		
-		// check expected
-		assertTrue(((LineString) new WKTReader().read(EXPECTED_LINESTRING)).equalsExact(simplifiedLS));
-		
-	}
+    @Test
+    public void getGeometryFactory() {
+        GeometryFactory gf = Utils.getGeometryFactory();
+        assertNotNull(gf);
+    }
+
+    @Test
+    public void setPrecision() {
+        GeometryFactory gf1 = Utils.getGeometryFactory();
+
+        PrecisionModel pm = new PrecisionModel(100.0);
+        Utils.setPrecision(pm);
+        GeometryFactory gf2 = Utils.getGeometryFactory();
+
+        assertNotSame(pm, gf1.getPrecisionModel());
+        assertSame(pm, gf2.getPrecisionModel());
+    }
+
+    @Test
+    public void testLineString() throws ParseException {
+        final LineString ls = (LineString) new WKTReader().read(LINESTRING);
+
+        // simplify
+        final LineString simplifiedLS = Utils.removeCollinearVertices(ls);
+
+        // check expected
+        assertTrue(((LineString) new WKTReader().read(EXPECTED_LINESTRING)).equalsExact(simplifiedLS));
+    }
+
+    @Test
+    public void testLinePolygon() throws ParseException {
+        final Polygon poly = (Polygon) new WKTReader().read(POLYGON);
+
+        // simplify
+        final Polygon simplifiedPoly = Utils.removeCollinearVertices(poly);
+
+        // check expected
+        assertTrue(((Polygon) new WKTReader().read(EXPECTED_POLYGON)).equalsExact(simplifiedPoly));
+    }
+
+    @Test
+    public void testLineRing() throws ParseException {
+        final LinearRing ring = (LinearRing) new WKTReader().read(RING);
+
+        // simplify
+        final LinearRing simplifiedring = (LinearRing) Utils.removeCollinearVertices(ring);
+
+        // check expected
+        assertTrue(((LinearRing) new WKTReader().read(EXPECTED_RING)).equalsExact(simplifiedring));
+    }
+
+    @Test
+    public void testDegenerate() throws ParseException {
+        final LineString original = (LineString) new WKTReader().read(DEGENERATE_LS);
+
+        // simplify
+        final LineString simplified = (LineString) Utils.removeCollinearVertices(original);
+
+        // check expected
+        assertTrue(simplified.equalsExact(original));
+    }
+
+    @Test
+    public void testGeometry() throws ParseException {
+        final Geometry ring = new WKTReader().read(RING);
+
+        // simplify
+        final LinearRing simplifiedring = (LinearRing) Utils.removeCollinearVertices(ring);
+
+        // check expected
+        assertTrue(((LinearRing) new WKTReader().read(EXPECTED_RING)).equalsExact(simplifiedring));
+
+
+        final Geometry poly = new WKTReader().read(POLYGON);
+
+        // simplify
+        final Polygon simplifiedPoly = (Polygon) Utils.removeCollinearVertices(poly);
+
+        // check expected
+        assertTrue(((Polygon) new WKTReader().read(EXPECTED_POLYGON)).equalsExact(simplifiedPoly));
+
+
+        final Geometry ls = new WKTReader().read(LINESTRING);
+
+        // simplify
+        final LineString simplifiedLS = (LineString) Utils.removeCollinearVertices(ls);
+
+        // check expected
+        assertTrue(((LineString) new WKTReader().read(EXPECTED_LINESTRING)).equalsExact(simplifiedLS));
+
+    }
 }
