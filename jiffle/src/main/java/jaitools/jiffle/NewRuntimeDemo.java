434a19bfd441cf50174c812592d9f62d59a5456b
michael.bedward <michael.bedward@07dc4d45-bd82-665a-fa80-c0abab6b2a50>
2011-01-21 06:23:47 +0000

    methods for running via JiffleExecutor or direct use of JiffleRuntime
    git-svn-id: https://jaitools.googlecode.com/svn/trunk@1314 07dc4d45-bd82-665a-fa80-c0abab6b2a50

--- jiffle/src/main/java/jaitools/jiffle/NewRuntimeDemo.java
+++ jiffle/src/main/java/jaitools/jiffle/NewRuntimeDemo.java
@@ -6,19 +6,31 @@ import jaitools.jiffle.runtime.JiffleEvent;
 import jaitools.jiffle.runtime.JiffleEventListener;
 import jaitools.jiffle.runtime.JiffleExecutor;
 import jaitools.jiffle.runtime.JiffleExecutorResult;
+import jaitools.jiffle.runtime.JiffleRuntime;
 import jaitools.jiffle.runtime.NullProgressListener;
 import jaitools.swing.ImageFrame;
 import java.awt.image.RenderedImage;
+import java.awt.image.WritableRenderedImage;
 
 import java.util.Map;
 import javax.swing.SwingUtilities;
 
 public class NewRuntimeDemo implements JiffleEventListener {
+    
+    // script from the ripple demo
+    private final String script =
+            "xc = width() / 2; \n"
+            + "yc = height() / 2; \n"
+            + "dx = (x()-xc)/xc; \n"
+            + "dy = (y()-yc)/yc; \n"
+            + "d = sqrt(dx^2 + dy^2); \n"
+            + "dest = sin(8 * M_PI * d); \n";
 
     private JiffleExecutor executor;
     
     public static void main(String[] args) throws Exception {
         NewRuntimeDemo me = new NewRuntimeDemo();
+        // me.directRuntimeDemo();
         me.executorDemo();
     }
     
@@ -26,23 +38,29 @@ public class NewRuntimeDemo implements JiffleEventListener {
         executor = new JiffleExecutor();
     }
     
+    private void directRuntimeDemo() throws Exception {
+        Map<String, Jiffle.ImageRole> params = CollectionFactory.map();
+        params.put("dest", Jiffle.ImageRole.DEST);
+        
+        Jiffle jiffle = new Jiffle(script, params);
+        
+        JiffleRuntime runtime = jiffle.getRuntimeInstance();
+        WritableRenderedImage destImg = ImageUtils.createConstantImage(500, 500, 0d);
+        runtime.setDestinationImage("dest", destImg);
+        runtime.evaluateAll(new NullProgressListener());
+        
+        displayImage(destImg);
+        System.out.println(jiffle.getRuntimeSource(true));
+    }
+    
     private void executorDemo() throws Exception {
         Map<String, Jiffle.ImageRole> params = CollectionFactory.map();
-        params.put("out", Jiffle.ImageRole.DEST);
+        params.put("dest", Jiffle.ImageRole.DEST);
         
-        // script from the ripple demo
-        String src = 
-                  "xc = width() / 2; \n"
-                + "yc = height() / 2; \n"
-                + "dx = (x()-xc)/xc; \n"
-                + "dy = (y()-yc)/yc; \n"
-                + "d = sqrt(dx^2 + dy^2); \n"
-                + "out = sin(8 * M_PI * d); \n";
-
-        Jiffle jiffle = new Jiffle(src, params);
+        Jiffle jiffle = new Jiffle(script, params);
         
         Map<String, RenderedImage> images = CollectionFactory.map();
-        images.put("out", ImageUtils.createConstantImage(500, 500, Double.valueOf(0)));
+        images.put("dest", ImageUtils.createConstantImage(500, 500, Double.valueOf(0)));
 
         if (!executor.isListening(this)) {
             executor.addEventListener(this);
@@ -54,20 +72,24 @@ public class NewRuntimeDemo implements JiffleEventListener {
         JiffleExecutorResult result = ev.getResult();
         System.out.println( result.getJiffle().getRuntimeSource(true) );
         
-        final RenderedImage img = result.getImages().get("out");
-        
+        RenderedImage img = result.getImages().get("dest");
+        displayImage(img);
+    }
+
+    public void onFailureEvent(JiffleEvent ev) {
+        System.out.println("Execution failed");
+        System.exit(0);
+    }
+
+    private void displayImage(final RenderedImage img) {
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
-                ImageFrame f = new ImageFrame(img, "test image");
+                ImageFrame f = new ImageFrame(img, "New runtime demo");
                 int w = img.getWidth();
                 f.setSize(w+50, w+50);
                 f.setVisible(true);
             }
         });
-    }
-
-    public void onFailureEvent(JiffleEvent ev) {
-        System.out.println("Execution failed");
     }
 
 }
