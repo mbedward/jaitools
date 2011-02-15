f8191d89e897afebccd12e59d12216484f0a49c4
michael.bedward <michael.bedward@07dc4d45-bd82-665a-fa80-c0abab6b2a50>
2011-02-15 22:24:18 +0000

    fixed typo in example script javadocs
    git-svn-id: https://jaitools.googlecode.com/svn/trunk@1404 07dc4d45-bd82-665a-fa80-c0abab6b2a50

--- demo/src/main/java/jaitools/demo/jiffle/package-info.java
+++ demo/src/main/java/jaitools/demo/jiffle/package-info.java
@@ -20,6 +20,23 @@
 
 /**
  * Example applications for the Jiffle scripting language. 
+ * <p>
+ * To see the various example scripts in action run the
+ * {@code JiffleDemo} program. It displays a script in a
+ * text window and the image produced by it in an adjacent
+ * window.
+ * <p>
+ * {@code JiffleBuilderDemo} shows how to compile and run scripts
+ * with {@code JiffleBuilder}. This is the easiest way to get
+ * started.
+ * <p>
+ * {@code DirectRuntimeDemo} demonstrates creating a Jiffle object
+ * directly, compiling a script, retrieving a runtime object and
+ * the using this to process images.
+ * <p>
+ * {@code JiffleExecutorDemo} show how to run Jiffle objects using
+ * a multi-threaded, event-driven executor service. This is especially
+ * useful for computationally intensive tasks.
  */
 
 package jaitools.demo.jiffle;
