dfef3116eb522f076eaba066700e959a2a4a3d03
michael.bedward <michael.bedward@07dc4d45-bd82-665a-fa80-c0abab6b2a50>
2011-03-02 12:19:52 +0000

    more unit tests for small polygon filtering
    git-svn-id: https://jaitools.googlecode.com/svn/trunk@1481 07dc4d45-bd82-665a-fa80-c0abab6b2a50

--- operator/vectorize/src/test/java/jaitools/media/jai/vectorize/SmallPolysTest.java
+++ operator/vectorize/src/test/java/jaitools/media/jai/vectorize/SmallPolysTest.java
@@ -21,6 +21,7 @@
 package jaitools.media.jai.vectorize;
 
 import java.awt.image.RenderedImage;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
@@ -62,11 +63,9 @@ public class SmallPolysTest extends TestBase {
         args = CollectionFactory.map();
     }
 
-    /**
-     * Tests deletion of small polygons
-     */
     @Test
     public void removeSmallPolys() throws Exception {
+        System.out.println("   remove small polygons");
         RenderedImage img = ImageUtils.createImageFromArray(DATA, WIDTH, WIDTH);
         
         args.put("filterThreshold", 1.1);
@@ -76,11 +75,9 @@ public class SmallPolysTest extends TestBase {
     }
     
     
-    /**
-     * Tests merging of small polygons into largest neighbour
-     */
     @Test
     public void mergeSmallPolysIntoLargestNbr() throws Exception {
+        System.out.println("   merge small polygons into largest neighbour");
         RenderedImage img = ImageUtils.createImageFromArray(DATA, WIDTH, WIDTH);
         
         args.put("filterThreshold", 1.1);
@@ -121,6 +118,37 @@ public class SmallPolysTest extends TestBase {
                     fail("Unexpected poly id");
             }
         }
+    }
+    
+    @Test
+    public void smallIslandPoly() throws Exception {
+        System.out.println("   small polygon surrounded by nodata");
+        
+        // Set 3 to nodata so that 4 will be an island poly
+        args.put("outsideValues", Collections.singleton(3d));
+        args.put("filterThreshold", 1.1);
+        args.put("filterMethod", VectorizeDescriptor.FILTER_MERGE_RANDOM);
+        
+        RenderedImage img = ImageUtils.createImageFromArray(DATA, WIDTH, WIDTH);
+        RenderedOp dest = doOp(img, args);
+        
+        // There should only be 2 polys because the island has no
+        // shared boundary segments
+        getPolygons(dest, 2);
+    }
+    
+    @Test
+    public void thresholdBelowMinPolySize() throws Exception {
+        System.out.println("   filter threshold below min poly size");
+        
+        args.put("filterThreshold", 0.5);
+        args.put("filterMethod", VectorizeDescriptor.FILTER_DELETE);
+        
+        RenderedImage img = ImageUtils.createImageFromArray(DATA, WIDTH, WIDTH);
+        RenderedOp dest = doOp(img, args);
+        
+        // There should be 7 polygons because none will have been filtered
+        getPolygons(dest, 7);
     }
     
     private Map<Integer, Integer> getStartAreas() {
