ea5ecfaf6f146b2f0f18ed94d53dd568f142b67f
michael.bedward <michael.bedward@07dc4d45-bd82-665a-fa80-c0abab6b2a50>
2011-03-06 10:20:28 +0000

    javadocs
    git-svn-id: https://jaitools.googlecode.com/svn/trunk@1508 07dc4d45-bd82-665a-fa80-c0abab6b2a50

--- jiffle/src/test/java/jaitools/jiffle/runtime/MockJiffle.java
+++ jiffle/src/test/java/jaitools/jiffle/runtime/MockJiffle.java
@@ -1,7 +1,23 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright 2011 Michael Bedward
+ * 
+ * This file is part of jai-tools.
+ *
+ * jai-tools is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the 
+ * License, or (at your option) any later version.
+ *
+ * jai-tools is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public 
+ * License along with jai-tools.  If not, see <http://www.gnu.org/licenses/>.
+ * 
  */
+
 package jaitools.jiffle.runtime;
 
 import jaitools.CollectionFactory;
@@ -10,30 +26,58 @@ import jaitools.jiffle.JiffleException;
 import java.util.Map;
 
 /**
- *
- * @author michael
+ * Mock object for unit tests.
+ * 
+ * @author Michael Bedward
+ * @since 1.1
+ * @version $Id$
  */
 public class MockJiffle extends Jiffle {
     
     private final int imageWidth;
     private final long pixelTime;
 
-    public MockJiffle(int imageWidth, long pixelTime) {
-        this.imageWidth = imageWidth;
+    /**
+     * Creates a new instance. The mock runtime object that can be
+     * retrieved from this will inherit the image size and pixel
+     * processing time values.
+     * 
+     * @param imageSize image size (number of pixels)
+     * @param pixelTime pixel processing time (mS)
+     * 
+     * @see MockJiffleRuntime
+     */
+    public MockJiffle(int imageSize, long pixelTime) {
+        this.imageWidth = imageSize;
         this.pixelTime = pixelTime;
     }
     
+    /**
+     * {@inheritDoc}
+     * @return always returns {@code true} for the mock object
+     */
     @Override
     public boolean isCompiled() {
         return true;
     }
 
+    /**
+     * {@inheritDoc}
+     * @return always returns an empty map
+     */
     @Override
     public Map<String, ImageRole> getImageParams() {
         Map<String, ImageRole> emptyParams = CollectionFactory.map();
         return emptyParams;
     }
 
+    /**
+     * Returns a new instance of {@link MockJiffleRuntime} initialized with
+     * the image size and pixel processing time values passed of this
+     * object.
+     * 
+     * @return the mock runtime object
+     */
     @Override
     public JiffleDirectRuntime getRuntimeInstance() throws JiffleException {
         return new MockJiffleRuntime(imageWidth, pixelTime);
--- jiffle/src/test/java/jaitools/jiffle/runtime/MockJiffleRuntime.java
+++ jiffle/src/test/java/jaitools/jiffle/runtime/MockJiffleRuntime.java
@@ -1,32 +1,66 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright 2011 Michael Bedward
+ * 
+ * This file is part of jai-tools.
+ *
+ * jai-tools is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the 
+ * License, or (at your option) any later version.
+ *
+ * jai-tools is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public 
+ * License along with jai-tools.  If not, see <http://www.gnu.org/licenses/>.
+ * 
  */
+
 package jaitools.jiffle.runtime;
 
 /**
- *
- * @author michael
+ * Mock object for unit tests.
+ * 
+ * @author Michael Bedward
+ * @since 1.1
+ * @version $Id$
  */
 class MockJiffleRuntime extends AbstractDirectRuntime {
 
     private final long pixelTime;
 
-    public MockJiffleRuntime(int imageWidth, long pixelTime) {
+    /**
+     * Creates a new mock object. Its thread will sleep for {@code pixelTime}
+     * milliseconds each time its {@link #evaluate(int, int)} method is 
+     * called.
+     * 
+     * @param imageSize image size (number of pixels)
+     * 
+     * @param pixelTime time to spend pretending to process each pixel
+     */
+    public MockJiffleRuntime(int imageSize, long pixelTime) {
         this.pixelTime = pixelTime;
         
         // super class fields
-        this._width = imageWidth;
+        this._width = imageSize;
         this._height = 1;
         this._minx = 0;
         this._miny = 0;
     }
 
 
+    /**
+     * Does nothing.
+     */
     @Override
     protected void initImageScopeVars() {
     }
 
+    /**
+     * Does nothing.
+     */
     @Override
     protected void initOptionVars() {
     }
