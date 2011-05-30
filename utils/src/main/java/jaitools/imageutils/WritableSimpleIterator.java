5ac8de262cb5d08e53e692d0c1a7c54bf4b02f18
michael.bedward <michael.bedward@07dc4d45-bd82-665a-fa80-c0abab6b2a50>
2011-05-30 06:48:18 +0000

    Issue 171: added setSample methods with position args to WritableSimplerIterator
    git-svn-id: https://jaitools.googlecode.com/svn/branches/1.2.x@1687 07dc4d45-bd82-665a-fa80-c0abab6b2a50

--- utils/src/main/java/jaitools/imageutils/WritableSimpleIterator.java
+++ utils/src/main/java/jaitools/imageutils/WritableSimpleIterator.java
@@ -20,6 +20,7 @@
 
 package jaitools.imageutils;
 
+import java.awt.Point;
 import java.awt.Rectangle;
 import java.awt.image.DataBuffer;
 import java.awt.image.RenderedImage;
@@ -78,11 +79,49 @@ public class WritableSimpleIterator extends AbstractSimpleIterator {
      * If the iterator is positioned outside the image bounds, no change is made
      * and this method returns {@code false}.
      * 
+     * @param value 
      * @return {@code true} if the image value was set; {@code false} if the 
      *     iterator was positioned outside the bounds of the image
      */
     public boolean setSample(Number value) {
         return setSample(0, value);
+    }
+
+    /**
+     * Sets the value in the first band of the image at the specified position.
+     * If the position lies outside the image bounds, no change is made
+     * and this method returns {@code false}.
+     * 
+     * @param pos the image position
+     * @param value the new value
+     * @return {@code true} if the image value was set; {@code false} if the 
+     *     specified position was outside the bounds of the image
+     * 
+     * @throws IllegalArgumentException if either argument is {@code null}
+     */
+    public boolean setSample(Point pos, Number value) {
+        if (pos == null) {
+            throw new IllegalArgumentException("pos must not be null");
+        }
+
+        return setSample(pos.x, pos.y, value);
+    }
+
+    /**
+     * Sets the value in the first band of the image at the specified position.
+     * If the position lies outside the image bounds, no change is made
+     * and this method returns {@code false}.
+     * 
+     * @param x image X-ordinate
+     * @param y image Y-ordinate
+     * @param value the new value
+     * @return {@code true} if the image value was set; {@code false} if the 
+     *     specified position was outside the bounds of the image
+     * 
+     * @throws IllegalArgumentException if {@code value} is {@code null}
+     */
+    public boolean setSample(int x, int y, Number value) {
+        return (setPos(x, y) && setSample(value));
     }
 
 
@@ -92,11 +131,12 @@ public class WritableSimpleIterator extends AbstractSimpleIterator {
      * and this method returns {@code false}.
      * 
      * @param band image band
+     * @param value the new value
      * @return {@code true} if the image value was set; {@code false} if the 
      *     iterator was positioned outside the bounds of the image
      * 
-     * @throws IllegalArgumentException if {@code band} is out of range for the the
-     *     target image
+     * @throws IllegalArgumentException if {@code band} is out of range for the 
+     *     target image or if {@code value} is {@code null}
      */
     public boolean setSample(int band, Number value) {
         if (value == null) {
@@ -129,6 +169,47 @@ public class WritableSimpleIterator extends AbstractSimpleIterator {
         } else {
             return false;
         }
+    }
+
+    /**
+     * Sets the value in the specified band of the image at the specified position.
+     * If the position lies outside the image bounds, no change is made
+     * and this method returns {@code false}.
+     * 
+     * @param pos the image position
+     * @param band the image band
+     * @param value the new value
+     * @return {@code true} if the image value was set; {@code false} if the 
+     *     specified position was outside the bounds of the image
+     * 
+     * @throws IllegalArgumentException if either {@code pos} or {@code value}
+     *     is {@code null}; or if {@code band} is out of range
+     */
+    public boolean setSample(Point pos, int band, Number value) {
+        if (pos == null) {
+            throw new IllegalArgumentException("pos must not be null");
+        }
+
+        return setSample(pos.x, pos.y, band, value);
+    }
+
+    /**
+     * Sets the value in the specified band of the image at the specified position.
+     * If the position lies outside the image bounds, no change is made
+     * and this method returns {@code false}.
+     * 
+     * @param x image X-ordinate
+     * @param y image Y-ordinate
+     * @param band the image band
+     * @param value the new value
+     * @return {@code true} if the image value was set; {@code false} if the 
+     *     specified position was outside the bounds of the image
+     * 
+     * @throws IllegalArgumentException if {@code value} is {@code null}; 
+     *     or if {@code band} is out of range
+     */
+    public boolean setSample(int x, int y, int band, Number value) {
+        return (setPos(x, y) && setSample(band, value));
     }
 
 }
