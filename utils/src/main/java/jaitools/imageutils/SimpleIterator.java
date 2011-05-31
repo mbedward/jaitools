c3162736d2ee8f35588ac351be0f06032322af5b
michael.bedward <michael.bedward@07dc4d45-bd82-665a-fa80-c0abab6b2a50>
2011-05-31 09:34:39 +0000

    Issue 172: add option for tile-wise iteration order to SimpleIterator and WritableSimpleIterator
    git-svn-id: https://jaitools.googlecode.com/svn/trunk@1690 07dc4d45-bd82-665a-fa80-c0abab6b2a50

--- utils/src/main/java/jaitools/imageutils/SimpleIterator.java
+++ utils/src/main/java/jaitools/imageutils/SimpleIterator.java
@@ -64,7 +64,25 @@ public class SimpleIterator extends AbstractSimpleIterator {
      *     the bounds of the target image; may be {@code null} 
      */
     public SimpleIterator(RenderedImage image, Rectangle bounds, Number outsideValue) {
-        super(new Helper(), image, bounds, outsideValue);
+        super(new Helper(), image, bounds, outsideValue, Order.IMAGE_X_Y);
+    }
+
+    /**
+     * Creates a new iterator. The bounds are allowed to extend beyond the bounds
+     * of the target image. When the iterator is positioned outside the image the
+     * specified outside value will be returned.
+     * 
+     * @param image the target image
+     * @param bounds bounds for the iterator; if {@code null} the bounds of the target
+     *     image will be used
+     * @param outsideValue value to return when the iterator is positioned beyond
+     *     the bounds of the target image; may be {@code null} 
+     * @param order processing order for this iterator when using the {@link #next()}
+     *     method
+     */
+    public SimpleIterator(RenderedImage image, Rectangle bounds, 
+            Number outsideValue, Order order) {
+        super(new Helper(), image, bounds, outsideValue, order);
     }
 
 }
