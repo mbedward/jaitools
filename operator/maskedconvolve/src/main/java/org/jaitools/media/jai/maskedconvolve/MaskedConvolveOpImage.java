d47dff25dbd36291f6f456652de3c8212ed17c96
michael.bedward <michael.bedward@07dc4d45-bd82-665a-fa80-c0abab6b2a50>
2011-06-23 05:45:36 +0000

    Minor code tweak to address FindBugs issue.
    git-svn-id: https://jaitools.googlecode.com/svn/trunk@1807 07dc4d45-bd82-665a-fa80-c0abab6b2a50

--- operator/maskedconvolve/src/main/java/org/jaitools/media/jai/maskedconvolve/MaskedConvolveOpImage.java
+++ operator/maskedconvolve/src/main/java/org/jaitools/media/jai/maskedconvolve/MaskedConvolveOpImage.java
@@ -225,7 +225,7 @@ public class MaskedConvolveOpImage extends AreaOpImage {
                     Double max = r.getMax().doubleValue();
                     Range<Double> rd = new Range<Double>(
                             min, r.isMinIncluded(), max, r.isMaxIncluded());
-                    this.noDataRanges.add((Range) oelem);
+                    this.noDataRanges.add(rd);
 
                 } else {
                     // This should have been picked up by validateParameters
