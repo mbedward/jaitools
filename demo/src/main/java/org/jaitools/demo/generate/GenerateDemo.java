06803b23fdc7947098142a0c43ef48e7c43b94de
mbedward <michael.bedward@gmail.com>
2013-01-23 17:09:08 +1100

    Another example for the Generate demo

--- demo/src/main/java/org/jaitools/demo/generate/GenerateDemo.java
+++ demo/src/main/java/org/jaitools/demo/generate/GenerateDemo.java
@@ -1,5 +1,6 @@
 package org.jaitools.demo.generate;
 
+import java.util.Random;
 import javax.media.jai.JAI;
 import javax.media.jai.ParameterBlockJAI;
 import javax.media.jai.RenderedOp;
@@ -15,11 +16,12 @@ import org.jaitools.swing.ImageFrame;
 public class GenerateDemo {
 
     public static void main(String[] args) {
-        new GenerateDemo().demo();
+        GenerateDemo me = new GenerateDemo();
+        me.uniformRandom();
+        me.rgbGradient();
     }
     
-    private void demo() {
-        
+    private void rgbGradient() {
         Generator diagonalGradient = new Generator() {
             public ImageDataType getDataType() {
                 return ImageDataType.BYTE;
@@ -42,7 +44,7 @@ public class GenerateDemo {
             }
             
             public Number getValue(int imageX, int imageY) {
-                return 128;
+                return value;
             }
         }
         
@@ -58,5 +60,28 @@ public class GenerateDemo {
         
         RenderedOp image = JAI.create("generate", pb);
         ImageFrame.showImage(image.createInstance(), "RGB Diagonal gradient");
+    }
+    
+    private void uniformRandom() {
+        class UniformRandomGenerator implements Generator {
+            private final Random rand = new Random();
+            
+            public ImageDataType getDataType() {
+                return ImageDataType.DOUBLE;
+            }
+
+            public Number getValue(int imageX, int imageY) {
+                return rand.nextDouble();
+            }
+        }
+
+        ParameterBlockJAI pb = new ParameterBlockJAI("generate");
+        pb.setParameter("width", 300);
+        pb.setParameter("height", 300);
+        
+        pb.setParameter("generators", new UniformRandomGenerator());
+        
+        RenderedOp image = JAI.create("generate", pb);
+        ImageFrame.showImage(image.createInstance(), "Single band uniform random");
     }
 }
